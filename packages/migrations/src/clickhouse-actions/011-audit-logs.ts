f7dd9e25f6dbaaadd50d212103a7ffe01a68b67c
Tuval Simha <tuval.simha@Gmail.com>
2024-11-12 15:33:36 +0200

    Update packages/migrations/src/clickhouse-actions/011-audit-logs.ts
    Co-authored-by: Kamil Kisiela <kamil.kisiela@gmail.com>

--- packages/migrations/src/clickhouse-actions/011-audit-logs.ts
+++ packages/migrations/src/clickhouse-actions/011-audit-logs.ts
@@ -2,19 +2,24 @@ import type { Action } from '../clickhouse';
 
 export const action: Action = async exec => {
   await exec(`
-    CREATE TABLE IF NOT EXISTS "audit_log"
-    (
-      "id" UUID DEFAULT generateUUIDv4() CODEC(ZSTD(1)),
-      "event_time" DateTime CODEC(ZSTD(1)),
-      "user_id" String CODEC(ZSTD(1)),
-      "user_email" String CODEC(ZSTD(1)),
-      "organization_id" String CODEC(ZSTD(1)),
-      "event_action" String CODEC(ZSTD(1)),
-      "metadata" String CODEC(ZSTD(1))
+    CREATE TABLE IF NOT EXISTS "audit_logs" (
+      id String CODEC(ZSTD(1)),
+      timestamp DateTime('UTC') CODEC(DoubleDelta, LZ4),
+      organization_id LowCardinality(String) CODEC(ZSTD(1)),
+      event_action LowCardinality(String) CODEC(ZSTD(1)),
+      user_id String CODEC(ZSTD(1)),
+      user_email String CODEC(ZSTD(1)),
+      metadata String CODEC(ZSTD(1)),
+      INDEX idx_action event_action TYPE set(0) GRANULARITY 1,
+      INDEX idx_user_id user_id TYPE bloom_filter(0.001) GRANULARITY 1,
     )
-    ENGINE = ReplacingMergeTree
-    ORDER BY ("organization_id")
-    TTL event_time + INTERVAL 1 YEAR
-    SETTINGS index_granularity = 8192
+    ENGINE = MergeTree
+    PARTITION BY toYearWeek(timestamp, 1, 'UTC')
+    ORDER BY (organization_id, timestamp)
+    TTL timestamp + INTERVAL 1 YEAR
+    SETTINGS index_granularity = 8192, ttl_only_drop_parts = 1;
   `);
+  // Because we partition data by weeks,
+  // I added `ttl_only_drop_parts` to simply drop parts,
+  // instead of rows when TTL is exceeded.
 };
