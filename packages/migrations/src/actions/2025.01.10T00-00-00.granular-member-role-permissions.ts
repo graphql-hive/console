e50b7d24641f13716d82c27d79bc56a738eb59e3
Laurin Quast <laurinquast@googlemail.com>
2024-12-30 11:44:47 +0100

    feat: database migration

---
+++ packages/migrations/src/actions/2025.01.10T00-00-00.granular-member-role-permissions.ts
@@ -0,0 +1,11 @@
+import { type MigrationExecutor } from '../pg-migrator';
+
+export default {
+  name: '2025-01-30T00-00-00.granular-member-role-permissions.ts',
+  run: ({ sql }) => sql`
+    ALTER TABLE "organization_member_roles"
+      ALTER "scopes" DROP NOT NULL
+      , ADD COLUMN "permissions" text[]
+    ;
+  `,
+} satisfies MigrationExecutor;
