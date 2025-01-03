9bcb99e5aa67600aad1f8b6926bfff6a66641b46
Dotan Simha <dotansimha@gmail.com>
2025-01-03 14:57:55 +0700

    added missing index for `targets` table and improve `getMaybeLatestVersion` call

---
+++ packages/migrations/src/actions/2025.01.03T00-00-00.improve-targets-index.ts
@@ -0,0 +1,13 @@
+import { type MigrationExecutor } from '../pg-migrator';
+
+// https://github.com/graphql-hive/console/issues/6249
+export default {
+  name: '2025.01.03T00-00-00.improve-targets-index.ts',
+  noTransaction: true,
+  run: ({ sql }) => [
+    {
+      name: `create "targets"."idx_targets_project_id_id" lookup index`,
+      query: sql`CREATE INDEX CONCURRENTLY idx_targets_project_id_id ON targets(project_id, id);`,
+    },
+  ],
+} satisfies MigrationExecutor;
