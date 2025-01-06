672d73fedae0bdac339dad49118bde6ea8562011
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-06 15:17:55 +0100

    Margins and column nav in /ecosystem

--- packages/web/docs/src/components/ecosystem-page/page.tsx
+++ packages/web/docs/src/components/ecosystem-page/page.tsx
@@ -7,7 +7,7 @@ export default function EcosystemPage() {
   return (
     <LandingPageContainer className="text-green-1000 light mx-auto max-w-[90rem] overflow-hidden [&>:not(header)]:px-4 lg:[&>:not(header)]:px-8 xl:[&>:not(header)]:px-[120px]">
       <EcosystemPageContent components={components} />
-      <GotAnIdeaSection />
+      <GotAnIdeaSection className="mx-4 md:mx-6" />
     </LandingPageContainer>
   );
 }
