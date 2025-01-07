b7d998114e51cf43fff2f090fc77f87b5f388c52
Jason Kuhrt <jasonkuhrt@me.com>
2025-01-07 09:47:52 -0500

    remove commented out fixtures

--- integration-tests/testkit/test.ts
+++ integration-tests/testkit/test.ts
@@ -8,27 +8,6 @@ import { TmpFile, tmpFile } from './fs';
 
 interface Context {
   sdlFile: TmpFile;
-  // seed: Seed;
-  // owner: OwnerSeed;
-  // org: OrgSeed;
-  // //
-  // // "single" branch
-  // //
-  // projectSingle: ProjectSeed;
-  // tokenForProjectSingle: TargetAccessTokenSeed;
-  // cliForProjectSingle: CLI;
-  // //
-  // // "federation" branch
-  // //
-  // projectFederation: ProjectSeed;
-  // tokenForProjectFederation: TargetAccessTokenSeed;
-  // cliForProjectFederation: CLI;
-  // //
-  // // "stitching" branch
-  // //
-  // projectStitching: ProjectSeed;
-  // tokenForProjectStitching: TargetAccessTokenSeed;
-  // cliForProjectStitching: CLI;
 }
 
 export const test = testBase.extend<Context>({
@@ -36,70 +15,4 @@ export const test = testBase.extend<Context>({
     const sdlFile = tmpFile('graphql');
     await use(sdlFile);
   },
-  // seed: async ({}, use) => {
-  //   const seed = await initSeed();
-  //   await use(seed);
-  // },
-  // owner: async ({ seed }, use) => {
-  //   const owner = await seed.createOwner();
-  //   await use(owner);
-  // },
-  // org: async ({ owner }, use) => {
-  //   const org = await owner.createOrg();
-  //   await use(org);
-  // },
-  // //
-  // // "single" branch
-  // //
-  // projectSingle: async ({ org }, use) => {
-  //   const project = await org.createProject(ProjectType.Single);
-  //   await use(project);
-  // },
-  // tokenForProjectSingle: async ({ projectSingle }, use) => {
-  //   const token = await projectSingle.createTargetAccessToken({});
-  //   await use(token);
-  // },
-  // cliForProjectSingle: async ({ tokenForProjectSingle }, use) => {
-  //   const cli = createCLI({
-  //     readwrite: tokenForProjectSingle.secret,
-  //     readonly: tokenForProjectSingle.secret,
-  //   });
-  //   await use(cli);
-  // },
-  // //
-  // // "federation" branch
-  // //
-  // projectFederation: async ({ org }, use) => {
-  //   const project = await org.createProject(ProjectType.Federation);
-  //   await use(project);
-  // },
-  // tokenForProjectFederation: async ({ projectFederation }, use) => {
-  //   const token = await projectFederation.createTargetAccessToken({});
-  //   await use(token);
-  // },
-  // cliForProjectFederation: async ({ tokenForProjectFederation }, use) => {
-  //   const cli = createCLI({
-  //     readwrite: tokenForProjectFederation.secret,
-  //     readonly: tokenForProjectFederation.secret,
-  //   });
-  //   await use(cli);
-  // },
-  // //
-  // // "stitching" branch
-  // //
-  // projectStitching: async ({ org }, use) => {
-  //   const project = await org.createProject(ProjectType.Stitching);
-  //   await use(project);
-  // },
-  // tokenForProjectStitching: async ({ projectStitching }, use) => {
-  //   const token = await projectStitching.createTargetAccessToken({});
-  //   await use(token);
-  // },
-  // cliForProjectStitching: async ({ tokenForProjectStitching }, use) => {
-  //   const cli = createCLI({
-  //     readwrite: tokenForProjectStitching.secret,
-  //     readonly: tokenForProjectStitching.secret,
-  //   });
-  //   await use(cli);
-  // },
 });
