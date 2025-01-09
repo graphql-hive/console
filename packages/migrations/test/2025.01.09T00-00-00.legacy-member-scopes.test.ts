8edbb2776ff55d566f51b19390ded26498679a2c
Kamil Kisiela <kamil.kisiela@gmail.com>
2025-01-09 19:03:14 +0100

    Simplify the test

--- packages/migrations/test/2025.01.09T00-00-00.legacy-member-scopes.test.ts
+++ packages/migrations/test/2025.01.09T00-00-00.legacy-member-scopes.test.ts
@@ -1,6 +1,6 @@
 import assert from 'node:assert';
 import { describe, test } from 'node:test';
-import { ForeignKeyIntegrityConstraintViolationError, sql } from 'slonik';
+import { sql } from 'slonik';
 import { createStorage } from '../../services/storage/src/index';
 import { initMigrationTestingEnvironment } from './utils/testkit';
 
@@ -38,59 +38,10 @@ await describe('migration: legacy-member-socpes', async () => {
         },
         user: admin,
       });
-      const secondaryAdmin = await seed.user({
-        user: {
-          name: 'test3',
-          email: 'test3@test.com',
-        },
-      });
-      const secondaryOrganization = await seed.organization({
-        organization: {
-          name: 'org-2',
-        },
-        user: secondaryAdmin,
-      });
 
-      const adminScopes = [
-        'organization:read',
-        'organization:delete',
-        'organization:settings',
-        'organization:integrations',
-        'organization:members',
-        'project:read',
-        'project:delete',
-        'project:settings',
-        'project:alerts',
-        'project:operations-store:read',
-        'project:operations-store:write',
-        'target:read',
-        'target:delete',
-        'target:settings',
-        'target:registry:read',
-        'target:registry:write',
-        'target:tokens:read',
-        'target:tokens:write',
-      ];
-      const contributorScopes = [
-        'organization:read',
-        'project:read',
-        'project:settings',
-        'project:alerts',
-        'project:operations-store:read',
-        'project:operations-store:write',
-        'target:read',
-        'target:settings',
-        'target:registry:read',
-        'target:registry:write',
-        'target:tokens:read',
-        'target:tokens:write',
-      ];
-      const noRoleUserScopes = [
-        'organization:read',
-        'project:read',
-        'target:read',
-        'project:alerts',
-      ];
+      const adminScopes = ['target:read', 'target:delete', 'target:settings', 'taret:write'];
+      const contributorScopes = ['target:read', 'taret:write'];
+      const noRoleUserScopes = ['target:read', 'target:settings'];
 
       // Create an invitation to simulate a pending invitation
       await db.query(sql`
@@ -102,8 +53,7 @@ await describe('migration: legacy-member-socpes', async () => {
         VALUES
           (${organization.id}, ${admin.id}, ${sql.array(adminScopes, 'text')}),
           (${organization.id}, ${contributor.id}, ${sql.array(contributorScopes, 'text')}),
-          (${organization.id}, ${noRoleUser.id}, ${sql.array(noRoleUserScopes, 'text')}),
-          (${secondaryOrganization.id}, ${secondaryAdmin.id}, ${sql.array(adminScopes, 'text')})
+          (${organization.id}, ${noRoleUser.id}, ${sql.array(noRoleUserScopes, 'text')})
       `);
 
       // assert correct scopes
@@ -124,12 +74,6 @@ await describe('migration: legacy-member-socpes', async () => {
         SELECT scopes FROM organization_member WHERE user_id = ${noRoleUser.id}
         `),
         noRoleUserScopes,
-      );
-      assert.deepStrictEqual(
-        await db.oneFirst(sql`
-        SELECT scopes FROM organization_member WHERE user_id = ${secondaryAdmin.id}
-        `),
-        adminScopes,
       );
 
       // run the next migrations
@@ -153,12 +97,6 @@ await describe('migration: legacy-member-socpes', async () => {
         SELECT scopes FROM organization_member WHERE user_id = ${noRoleUser.id}
         `),
         noRoleUserScopes,
-      );
-      assert.deepStrictEqual(
-        await db.oneFirst(sql`
-        SELECT scopes FROM organization_member WHERE user_id = ${secondaryAdmin.id}
-        `),
-        adminScopes,
       );
 
       // assert assigned roles have identical scopes
@@ -191,91 +129,6 @@ await describe('migration: legacy-member-socpes', async () => {
         SELECT role_id FROM organization_member WHERE user_id = ${noRoleUser.id}
         `),
         null,
-      );
-
-      assert.deepStrictEqual(
-        await db.oneFirst(sql`
-        SELECT omr.scopes
-        FROM organization_member as om
-        LEFT JOIN organization_member_roles as omr ON omr.id = om.role_id
-        WHERE om.user_id = ${secondaryAdmin.id} AND omr.organization_id = ${secondaryOrganization.id}
-        `),
-        adminScopes,
-      );
-
-      // deleting a role with assigned members should not be possible
-      const deletionError = await db
-        .query(
-          sql`
-          DELETE FROM organization_member_roles WHERE organization_id = ${organization.id} AND id IN (
-            SELECT role_id FROM organization_member WHERE organization_id = ${organization.id} AND user_id = ${contributor.id}
-          )
-      `,
-        )
-        .catch(error => Promise.resolve(error));
-      assert.strictEqual(
-        deletionError instanceof ForeignKeyIntegrityConstraintViolationError,
-        true,
-      );
-
-      // locked roles should be Viewer and Admin
-      assert.deepStrictEqual(
-        await db.manyFirst(sql`
-          SELECT name FROM organization_member_roles WHERE organization_id = ${organization.id} AND locked = true ORDER BY name ASC
-        `),
-        ['Admin', 'Viewer'],
-      );
-
-      // deleting a member should not delete the role
-      const contributorRoleId = await db.oneFirst<string>(sql`
-        SELECT role_id FROM organization_member WHERE organization_id = ${organization.id} AND user_id = ${contributor.id}
-      `);
-      assert.strictEqual(
-        await db.oneFirst(sql`
-          DELETE FROM organization_member WHERE user_id = ${contributor.id} AND organization_id = ${organization.id} RETURNING user_id
-        `),
-        contributor.id,
-      );
-      assert.strictEqual(
-        await db.oneFirst(sql`
-          SELECT id FROM organization_member_roles WHERE organization_id = ${organization.id} AND id = ${contributorRoleId}
-        `),
-        contributorRoleId,
-      );
-
-      // deleting a user should not delete the role
-      const customRoleId = await db.oneFirst<string>(sql`
-        INSERT INTO organization_member_roles
-        (organization_id, name, description, scopes)
-        VALUES
-        (${organization.id}, 'Custom', 'Custom role', ${sql.array(noRoleUserScopes, 'text')})
-        RETURNING id
-      `);
-      await db.query(sql`
-        UPDATE organization_member SET role_id = ${customRoleId} WHERE user_id = ${noRoleUser.id} AND organization_id = ${organization.id}
-      `);
-      assert.strictEqual(
-        await db.oneFirst(sql`
-          DELETE FROM users WHERE id = ${noRoleUser.id} RETURNING id
-        `),
-        noRoleUser.id,
-      );
-      assert.strictEqual(
-        await db.oneFirst(sql`
-          SELECT id FROM organization_member_roles WHERE organization_id = ${organization.id} AND id = ${customRoleId}
-        `),
-        customRoleId,
-      );
-
-      // pending invitations should have Viewer role
-      assert.deepStrictEqual(
-        await db.manyFirst(sql`
-          SELECT omr.name
-          FROM organization_invitations as oi
-          LEFT JOIN organization_member_roles as omr ON (omr.id = oi.role_id AND omr.organization_id = oi.organization_id)
-          WHERE oi.organization_id = ${organization.id}
-        `),
-        ['Viewer'],
       );
 
       await complete();
