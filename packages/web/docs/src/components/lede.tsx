9fbdcd46a061036d1d2e819dbae8f47d44204b57
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-13 22:42:25 +0100

    Remove pt-2 from Lede, because TOC was removed

--- packages/web/docs/src/components/lede.tsx
+++ packages/web/docs/src/components/lede.tsx
@@ -3,6 +3,5 @@ import { cn } from '@theguild/components';
 export interface LedeProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 
 export function Lede(props: LedeProps) {
-  // pt-2 is temporary -- it aligns the text with TOC
-  return <div {...props} className={cn('pt-2 sm:*:text-xl/6 md:*:text-2xl/8', props.className)} />;
+  return <div {...props} className={cn('sm:*:text-xl/6 md:*:text-2xl/8', props.className)} />;
 }
