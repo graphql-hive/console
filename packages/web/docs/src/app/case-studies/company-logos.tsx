ccb69b8552c62bd57a90015f8dc82507e47cf4a5
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-14 00:46:11 +0100

    Add Wealthsimple markdown

--- packages/web/docs/src/app/case-studies/company-logos.tsx
+++ packages/web/docs/src/app/case-studies/company-logos.tsx
@@ -1,5 +1,6 @@
-import { SoundYXZLogo } from '../../components/company-logos';
+import { SoundYXZLogo, WealthsimpleLogo } from '../../components/company-logos';
 
 export const companyLogos = {
   'sound-xyz': <SoundYXZLogo width={193} height={64} />,
+  wealthsimple: <WealthsimpleLogo width={212} height={64} />,
 };
