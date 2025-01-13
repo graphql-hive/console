ad416555d2c9d68aba65f6cfe09a809854a3fbdf
Jason Kuhrt <jasonkuhrt@me.com>
2025-01-13 11:00:09 -0500

    minimize ns files

--- packages/libraries/cli/src/helpers/typebox/_exports.ts
+++ packages/libraries/cli/src/helpers/typebox/_exports.ts
@@ -2,6 +2,6 @@
 import './formats/uri';
 
 export * from '@sinclair/typebox';
-export * from './value/_namespace';
+export * as Value from './value/_exports';
 export * from './types/Nullable';
 export * from './types/StringNonEmpty';
