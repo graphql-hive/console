9cbe6ae0d882c0a1843ac2aa717dff4c19e1689c
Tom Hastjarjanto <tom@mailx.nl>
2025-01-16 21:30:57 +0100

    chore: update environment.ts files

--- packages/services/storage/src/db/utils.ts
+++ packages/services/storage/src/db/utils.ts
@@ -3,20 +3,20 @@ import { sql } from 'slonik';
 export function createConnectionString(config: {
   host: string;
   port: number;
-  password: string;
+  password: string | undefined;
   user: string;
   db: string;
   ssl: boolean;
 }) {
   // prettier-ignore
   const encodedUser = encodeURIComponent(config.user);
-  const encodedPassword = typeof config.password === 'string' ? `:${encodeURIComponent(config.password)}` : '';
+  const encodedPassword =
+    typeof config.password === 'string' ? `:${encodeURIComponent(config.password)}` : '';
   const encodedHost = encodeURIComponent(config.host);
   const encodedDb = encodeURIComponent(config.db);
 
   return `postgres://${encodedUser}${encodedPassword}@${encodedHost}:${config.port}/${encodedDb}${config.ssl ? '?sslmode=require' : '?sslmode=disable'}`;
 }
-
 
 export function objectToParams<T extends Record<string, any>>(
   obj: T,
