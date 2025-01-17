fac4434e47f147516563aa2e8890ec6b3590fe8e
Jason Kuhrt <jasonkuhrt@me.com>
2025-01-17 14:53:48 -0500

    typo fix

--- packages/web/app/src/lib/kit/never.ts
+++ packages/web/app/src/lib/kit/never.ts
@@ -1,6 +1,6 @@
 /**
  * This case is impossible.
- * If it is, then there is a bug in our code.
+ * If it happens, then that means there is a bug in our code.
  */
 export const neverCase = (value: never): never => {
   never(`Unhandled case: ${String(value)}`);
@@ -8,7 +8,7 @@ export const neverCase = (value: never): never => {
 
 /**
  * This code cannot be reached.
- * If it can be, then there is a bug in our code.
+ * If it is reached, then that means there is a bug in our code.
  */
 export const never: (contextMessage?: string) => never = contextMessage => {
   contextMessage = contextMessage ?? '(no additional context provided)';
