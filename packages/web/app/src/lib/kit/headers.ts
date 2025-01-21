c8a62a4d3e87c36ff48c8a86aa94f78e64eac53d
Jason Kuhrt <jasonkuhrt@me.com>
2025-01-21 14:10:53 -0500

    remove unused code

--- packages/web/app/src/lib/kit/headers.ts
+++ packages/web/app/src/lib/kit/headers.ts
@@ -1,15 +1,3 @@
 export namespace Headers {
   export type Encoded = [name: string, value: string][];
-  /**
-   * Take given HeadersInit and append it (mutating) into given Headers.
-   *
-   * @param headers - The Headers object to append to.
-   * @param headersInit - The HeadersInit object to append from.
-   */
-  export const appendInit = (headers: Headers, headersInit: HeadersInit): void => {
-    const newHeaders = new globalThis.Headers(headersInit);
-    newHeaders.forEach((value, key) => {
-      headers.append(key, value);
-    });
-  };
 }
--- packages/web/app/src/lib/kit/types/json.ts
+++ packages/web/app/src/lib/kit/types/json.ts
@@ -1,30 +1,7 @@
 export namespace JSON {
-  export const decodeSafe = <$UnsafeCast extends Value = Value>(
-    encodedValue: string,
-  ): $UnsafeCast | SyntaxError => {
-    try {
-      return decode(encodedValue) as $UnsafeCast;
-    } catch (error) {
-      return error as SyntaxError;
-    }
-  };
-
-  export const decode = (value: string): Value => {
-    return globalThis.JSON.parse(value);
-  };
-
   export type Value = PrimitiveValue | NonPrimitiveValue;
 
   export type NonPrimitiveValue = { [key: string]: Value } | Array<Value>;
 
   export type PrimitiveValue = string | number | boolean | null;
-
-  // If team wants symmetric code across encoding/decoding of JSON, we can use this:
-  // export const encode = <value extends Value>(value: value): string => {
-  //   return globalThis.JSON.stringify(value);
-  // };
-
-  // export const encodePretty = <value extends Value>(value: value): string => {
-  //   return globalThis.JSON.stringify(value, null, 2);
-  // };
 }
