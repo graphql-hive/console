222fae73dcd232887d54fa0d484efaeb9916df30
Kamil Kisiela <kamil.kisiela@gmail.com>
2025-01-21 10:40:19 +0100

    do the db second

--- packages/migrations/src/actions/2025.01.17T10-08-00.drop-activities.ts
+++
@@ -1,8 +0,0 @@
-import { type MigrationExecutor } from '../pg-migrator';
-
-export default {
-  name: '2025.01.17T10-08-00.drop-activities.ts',
-  run: ({ sql }) => sql`
-    DROP TABLE IF EXISTS "activities";
-`,
-} satisfies MigrationExecutor;
