d6bf9adddbe9191c30b9d36dcf0720ca89c852ce
Jason Kuhrt <jasonkuhrt@me.com>
2025-01-22 14:48:41 -0500

    lint

--- packages/web/app/src/lib/kit/never.ts
+++ packages/web/app/src/lib/kit/never.ts
@@ -11,5 +11,5 @@ export const neverCase = (value: never): never => {
  * If it is reached, then that means there is a bug in our code.
  */
 export const never: (context?: object) => never = context => {
-  throw new Error(`Something that should be impossible happened`, { cause: context });
+  throw new Error('Something that should be impossible happened', { cause: context });
 };
