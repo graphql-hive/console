4783ace8ad57ca994b93faa5fc15e9eb4d2ab23a
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-02-03 16:50:08 +0100

    Add Gateway hero decoration

--- packages/web/docs/src/app/gateway/page.tsx
+++ packages/web/docs/src/app/gateway/page.tsx
@@ -1,14 +1,20 @@
 import { Metadata } from 'next';
-import { CallToAction, ExploreMainProductCards, Hero, HiveGatewayIcon } from '@theguild/components';
+import {
+  CallToAction,
+  DecorationIsolation,
+  ExploreMainProductCards,
+  Hero,
+  HeroLogo,
+  HiveGatewayIcon,
+} from '@theguild/components';
 import { LandingPageContainer } from '../../components/landing-page-container';
 import { metadata as rootMetadata } from '../layout';
 import { GatewayFeatureTabs } from './gateway-feature-tabs';
 
 export const metadata: Metadata = {
   title: 'Hive Gateway',
-  // TODO:
-  // description:
-  //   'Fully Open-Source schema registry, analytics and gateway for GraphQL federation and other GraphQL APIs',
+  description:
+    'Unify and accelerate your data graph with Hive Gateway, which seamlessly integrates with Apollo Federation.',
   alternates: {
     // to remove leading slash
     canonical: '.',
@@ -24,19 +30,23 @@ export default function HiveGatewayPage() {
   return (
     <LandingPageContainer className="text-green-1000 light mx-auto max-w-[90rem] overflow-hidden">
       <Hero
-        logo={<HiveGatewayIcon />}
+        top={
+          <HeroLogo>
+            <HiveGatewayIcon />
+          </HeroLogo>
+        }
         heading="Hive Gateway"
         text="Unify and accelerate your data graph across diverse services with Hive Gateway, which seamlessly integrates with Apollo Federation."
         checkmarks={['Fully open source', 'No vendor lock-in', 'Can be self-hosted!']}
         className="mx-4 md:mx-6"
       >
-        <CallToAction variant="primary" href="/docs/gateway">
+        <CallToAction variant="primary-inverted" href="/docs/gateway">
           Get Started
         </CallToAction>
-        <CallToAction variant="tertiary" href="https://github.com/graphql-hive/gateway">
+        <CallToAction variant="secondary-inverted" href="https://github.com/graphql-hive/gateway">
           GitHub
         </CallToAction>
-        {/* TODO: decoration */}
+        <GatewayHeroDecoration />
       </Hero>
       <GatewayFeatureTabs />
       {/* observability and performance monitoring */}
@@ -47,5 +57,38 @@ export default function HiveGatewayPage() {
       <ExploreMainProductCards className="max-lg:mx-4 max-lg:my-8" />
       {/* big get your API game right section */}
     </LandingPageContainer>
+  );
+}
+
+function GatewayHeroDecoration() {
+  return (
+    <DecorationIsolation className="-z-10">
+      <HiveGatewayIcon className="absolute left-[-268px] top-[-8px] size-[520px] fill-[url(#gateway-hero-gradient)] max-lg:hidden" />
+      <HiveGatewayIcon className="absolute right-[-144px] top-[-64px] size-[320px] fill-[url(#gateway-hero-gradient-mobile)] md:bottom-[-64px] md:right-[-268px] md:top-auto md:size-[520px] md:fill-[url(#gateway-hero-gradient)] lg:bottom-[-8px]" />
+      <svg
+        className="pointer-events-none -z-50 size-0"
+        width="192"
+        height="296"
+        viewBox="0 0 192 296"
+      >
+        <defs>
+          <linearGradient id="gateway-hero-gradient" gradientTransform="rotate(139)">
+            <stop offset="11.66%" stopColor="rgba(255, 255, 255, 0.1)" />
+            <stop offset="74.87%" stopColor="rgba(255, 255, 255, 0.3)" />
+          </linearGradient>
+          <linearGradient
+            id="gateway-hero-gradient-mobile"
+            x1="35.3488"
+            y1="15.0697"
+            x2="224.372"
+            y2="229.023"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="white" stopOpacity="0.2" />
+            <stop offset="80%" stopColor="white" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </DecorationIsolation>
   );
 }
