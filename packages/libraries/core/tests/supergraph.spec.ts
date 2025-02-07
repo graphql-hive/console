3fe6009507249ea9a786e75ebf1d6e7dfa58e292
Laurin Quast <laurinquast@googlemail.com>
2025-02-07 12:14:44 +0100

    update more fixtures

--- packages/libraries/core/tests/supergraph.spec.ts
+++ packages/libraries/core/tests/supergraph.spec.ts
@@ -1,4 +1,5 @@
 import nock from 'nock';
+import { maskRequestId } from 'test-utils.js';
 import { describe, expect, test } from 'vitest';
 import { createSupergraphSDLFetcher } from '../src/index.js';
 import { version } from '../src/version';
@@ -131,9 +132,9 @@ describe('supergraph SDL fetcher', async () => {
 
     try {
       await fetcher();
-    } catch (err) {
-      expect(err).toMatchInlineSnapshot(
-        `[Error: GET http://localhost/supergraph failed with status 500.]`,
+    } catch (err: any) {
+      expect(maskRequestId(err.message)).toMatchInlineSnapshot(
+        `GET http://localhost/supergraph (x-request-id=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx) failed with status 500.`,
       );
     }
   });
--- packages/libraries/core/tests/test-utils.ts
+++ packages/libraries/core/tests/test-utils.ts
@@ -9,17 +9,15 @@ function getLogLines(calls: Array<Array<unknown>>) {
   return calls.map(log => {
     let msg: string;
     if (typeof log[1] === 'string') {
-      msg = log[1]
-        // Replace milliseconds with static value
-        .replace(/\(\d{1,4}ms\)/, '(666ms)')
-        // Replace stack trace line numbers with static value
-        .replace(/\(node:net:\d+:\d+\)/, '(node:net:666:666)')
-        .replace(/\(node:dns:\d+:\d+\)/, '(node:dns:666:666)')
+      msg = maskRequestId(
+        log[1]
+          // Replace milliseconds with static value
+          .replace(/\(\d{1,4}ms\)/, '(666ms)')
+          // Replace stack trace line numbers with static value
+          .replace(/\(node:net:\d+:\d+\)/, '(node:net:666:666)')
+          .replace(/\(node:dns:\d+:\d+\)/, '(node:dns:666:666)'),
         // request UUIDsu
-        .replace(
-          /[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}/,
-          'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
-        );
+      );
     } else {
       msg = String(log[1]);
     }
@@ -40,4 +38,11 @@ export function createHiveTestingLogger() {
       fn = vi.fn();
     },
   };
+}
+
+export function maskRequestId(errorMessage: string) {
+  return errorMessage.replace(
+    /[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}/,
+    'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+  );
 }
