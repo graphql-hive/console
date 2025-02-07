c76190db79913e045ccb18dd7ca72f9f0b7fbc86
Laurin Quast <laurinquast@googlemail.com>
2025-02-07 15:36:40 +0100

    cli fix

--- packages/services/api/src/modules/schema/providers/schema-publisher.ts
+++ packages/services/api/src/modules/schema/providers/schema-publisher.ts
@@ -347,13 +347,6 @@ export class SchemaPublisher {
       this.logger.debug('No service name provided (type=%s)', project.type);
       increaseSchemaCheckCountMetric('rejected');
 
-      if (input.github) {
-        return {
-          __typename: 'GitHubSchemaCheckError' as const,
-          message: 'Missing service name provided.',
-        };
-      }
-
       return {
         __typename: 'SchemaCheckError',
         valid: false,
