2e129953bbbfdec197f8274228985e2d91c79a37
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-02-10 18:16:05 +0100

    Update packages/web/docs/src/components/error-boundary.ts

--- packages/web/docs/src/components/error-boundary.ts
+++ packages/web/docs/src/components/error-boundary.ts
@@ -10,7 +10,6 @@ export class ErrorBoundary extends Component<{
 
   static getDerivedStateFromError(error: Error) {
     console.error(error);
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
