161245ba5733d97dfe1f8afd28824adb6e910fe1
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-02-25 19:31:45 +0100

    Scroll to the highlighted card if it's not on screen

--- packages/web/docs/src/components/pricing/index.tsx
+++ packages/web/docs/src/components/pricing/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { HTMLAttributes, ReactElement, ReactNode, useState } from 'react';
+import { HTMLAttributes, ReactElement, ReactNode, useRef, useState } from 'react';
 import { Arrow, Content, Root, Trigger } from '@radix-ui/react-tooltip';
 import { CallToAction, cn, ContactButton, ContactTextLink, Heading } from '@theguild/components';
 import { PlanCard } from './plan-card';
@@ -34,6 +34,7 @@ export function Pricing({ className }: { className?: string }): ReactElement {
   type PlanType = 'Hobby' | 'Pro' | 'Enterprise';
 
   const [highlightedPlan, setHighlightedPlan] = useState<PlanType>('Hobby');
+  const scrollviewRef = useRef<HTMLDivElement>(null);
 
   return (
     <section className={cn('py-12 sm:py-20', className)}>
@@ -50,127 +51,152 @@ export function Pricing({ className }: { className?: string }): ReactElement {
           className="mt-6 lg:mt-12"
           onChange={value => {
             const newPlan = value === 1 ? 'Hobby' : value < 280 ? 'Pro' : 'Enterprise';
-            if (newPlan !== highlightedPlan) setHighlightedPlan(newPlan);
+            if (newPlan !== highlightedPlan) {
+              setHighlightedPlan(newPlan);
+              if (!scrollviewRef.current) return;
+              const card = scrollviewRef.current.querySelector(
+                `[data-plan="${newPlan}"]`,
+              ) as HTMLElement;
+              if (!card) return;
+
+              const isLeftSideInView = card.offsetLeft > scrollviewRef.current.scrollLeft;
+              const isRightSideInView =
+                card.offsetLeft + card.offsetWidth <
+                scrollviewRef.current.scrollLeft + scrollviewRef.current.offsetWidth;
+
+              console.log({
+                isLeftSideInView,
+                isRightSideInView,
+                offsetLeft: card.offsetLeft,
+                offsetWidth: card.offsetWidth,
+                scrollviewWidth: scrollviewRef.current.offsetWidth,
+              });
+
+              if (isLeftSideInView && isRightSideInView) return;
+
+              // todo: scroll to center card better
+
+              scrollviewRef.current.scrollTo({
+                left: card.offsetLeft,
+                behavior: 'smooth',
+              });
+            }
           }}
         />
 
         <div
+          ref={scrollviewRef}
           // the padding is here so `overflow-auto` doesn't cut button hover states
-          className="nextra-scrollbar -mx-2 -mb-6 overflow-auto px-2 py-6 lg:mt-6"
+          className="nextra-scrollbar -mx-2 -mb-6 flex snap-x snap-proximity flex-col items-stretch gap-6 overflow-auto px-2 py-6 *:snap-center sm:flex-row sm:*:min-w-[380px] lg:mt-6"
         >
-          <div
-            className={cn(
-              'flex min-w-[1208px] flex-col items-stretch gap-8 sm:grid sm:grid-cols-3 lg:gap-10 xl:gap-12',
-            )}
-          >
-            <PlanCard
-              name="Hobby"
-              description="For personal or small projects"
-              highlighted={highlightedPlan === 'Hobby'}
-              adjustable={false}
-              price="Free forever"
-              callToAction={
-                <CallToAction variant="tertiary" href="https://app.graphql-hive.com">
-                  Get started for free
-                </CallToAction>
-              }
-              features={
-                <>
-                  <PlanFeaturesListItem>
-                    <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
-                      <strong>7 days</strong> of usage data retention
-                    </Tooltip>
-                  </PlanFeaturesListItem>
-                  <li className="mb-2 mt-8">Includes:</li>
-                  <PlanFeaturesListItem>
-                    Unlimited seats, projects and organizations
-                  </PlanFeaturesListItem>
-                  <PlanFeaturesListItem>Unlimited schema pushes & checks</PlanFeaturesListItem>
-                  <PlanFeaturesListItem>
-                    Full access to all features (including&nbsp;SSO)
-                  </PlanFeaturesListItem>
-                  <PlanFeaturesListItem>
-                    <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
-                      1M operations per month
-                    </Tooltip>
-                  </PlanFeaturesListItem>
-                </>
-              }
-            />
-            <PlanCard
-              name="Pro"
-              description="For scaling API and teams"
-              highlighted={highlightedPlan === 'Pro'}
-              adjustable
-              price={
-                <Tooltip content="Base price charged monthly">
-                  $20<span className="text-base leading-normal text-green-800"> / month</span>
-                </Tooltip>
-              }
-              callToAction={
-                <CallToAction variant="primary" href="https://app.graphql-hive.com">
-                  Try free for 30 days
-                </CallToAction>
-              }
-              features={
-                <>
-                  <PlanFeaturesListItem>
-                    <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
-                      <strong>90 days</strong> of usage data retention
-                    </Tooltip>
-                  </PlanFeaturesListItem>
-                  <li className="mb-2 mt-8">Everything in Hobby, plus:</li>
-                  <PlanFeaturesListItem>
-                    <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
-                      $10 per additional 1M operations
-                    </Tooltip>
-                  </PlanFeaturesListItem>
-                </>
-              }
-            />
-            <PlanCard
-              name="Enterprise"
-              description="Custom plan for large companies"
-              highlighted={highlightedPlan === 'Enterprise'}
-              adjustable
-              price={
-                <ContactTextLink className="hover:text-current hover:no-underline">
-                  Contact us
-                </ContactTextLink>
-              }
-              callToAction={
-                <ContactButton variant="primary">
-                  Shape a custom plan for your business
-                </ContactButton>
-              }
-              features={
-                <>
-                  <PlanFeaturesListItem>
-                    <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
-                      <strong>Custom</strong> data retention
-                    </Tooltip>
-                  </PlanFeaturesListItem>
-                  <li className="mb-2 mt-8">Everything in Pro, plus:</li>
-                  <PlanFeaturesListItem>Dedicated Slack channel for support</PlanFeaturesListItem>
-                  <PlanFeaturesListItem>White-glove onboarding</PlanFeaturesListItem>
-                  <PlanFeaturesListItem>Bulk volume discount</PlanFeaturesListItem>
-                  <PlanFeaturesListItem>
-                    <span>
-                      GraphQL / APIs support and guidance from{' '}
-                      <a
-                        href="https://the-guild.dev"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="hive-focus -mx-1 -my-0.5 rounded px-1 py-0.5 underline hover:text-blue-700"
-                      >
-                        The&nbsp;Guild
-                      </a>
-                    </span>
-                  </PlanFeaturesListItem>
-                </>
-              }
-            />
-          </div>
+          <PlanCard
+            data-plan="Hobby"
+            name="Hobby"
+            description="For personal or small projects"
+            highlighted={highlightedPlan === 'Hobby'}
+            adjustable={false}
+            price="Free forever"
+            callToAction={
+              <CallToAction variant="tertiary" href="https://app.graphql-hive.com">
+                Get started for free
+              </CallToAction>
+            }
+            features={
+              <>
+                <PlanFeaturesListItem>
+                  <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
+                    <strong>7 days</strong> of usage data retention
+                  </Tooltip>
+                </PlanFeaturesListItem>
+                <li className="mb-2 mt-8">Includes:</li>
+                <PlanFeaturesListItem>
+                  Unlimited seats, projects and organizations
+                </PlanFeaturesListItem>
+                <PlanFeaturesListItem>Unlimited schema pushes & checks</PlanFeaturesListItem>
+                <PlanFeaturesListItem>
+                  Full access to all features (including&nbsp;SSO)
+                </PlanFeaturesListItem>
+                <PlanFeaturesListItem>
+                  <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                    1M operations per month
+                  </Tooltip>
+                </PlanFeaturesListItem>
+              </>
+            }
+          />
+          <PlanCard
+            data-plan="Pro"
+            name="Pro"
+            description="For scaling API and teams"
+            highlighted={highlightedPlan === 'Pro'}
+            adjustable
+            price={
+              <Tooltip content="Base price charged monthly">
+                $20<span className="text-base leading-normal text-green-800"> / month</span>
+              </Tooltip>
+            }
+            callToAction={
+              <CallToAction variant="primary" href="https://app.graphql-hive.com">
+                Try free for 30 days
+              </CallToAction>
+            }
+            features={
+              <>
+                <PlanFeaturesListItem>
+                  <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
+                    <strong>90 days</strong> of usage data retention
+                  </Tooltip>
+                </PlanFeaturesListItem>
+                <li className="mb-2 mt-8">Everything in Hobby, plus:</li>
+                <PlanFeaturesListItem>
+                  <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                    $10 per additional 1M operations
+                  </Tooltip>
+                </PlanFeaturesListItem>
+              </>
+            }
+          />
+          <PlanCard
+            data-plan="Enterprise"
+            name="Enterprise"
+            description="Custom plan for large companies"
+            highlighted={highlightedPlan === 'Enterprise'}
+            adjustable
+            price={
+              <ContactTextLink className="hover:text-current hover:no-underline">
+                Contact us
+              </ContactTextLink>
+            }
+            callToAction={
+              <ContactButton variant="primary">Shape a custom plan for your business</ContactButton>
+            }
+            features={
+              <>
+                <PlanFeaturesListItem>
+                  <Tooltip content={USAGE_DATA_RETENTION_EXPLAINER}>
+                    <strong>Custom</strong> data retention
+                  </Tooltip>
+                </PlanFeaturesListItem>
+                <li className="mb-2 mt-8">Everything in Pro, plus:</li>
+                <PlanFeaturesListItem>Dedicated Slack channel for support</PlanFeaturesListItem>
+                <PlanFeaturesListItem>White-glove onboarding</PlanFeaturesListItem>
+                <PlanFeaturesListItem>Bulk volume discount</PlanFeaturesListItem>
+                <PlanFeaturesListItem>
+                  <span>
+                    GraphQL / APIs support and guidance from{' '}
+                    <a
+                      href="https://the-guild.dev"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hive-focus -mx-1 -my-0.5 rounded px-1 py-0.5 underline hover:text-blue-700"
+                    >
+                      The&nbsp;Guild
+                    </a>
+                  </span>
+                </PlanFeaturesListItem>
+              </>
+            }
+          />
         </div>
       </div>
     </section>
--- packages/web/docs/src/components/pricing/plan-card.tsx
+++ packages/web/docs/src/components/pricing/plan-card.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, ReactNode } from 'react';
 import { cn } from '@theguild/components';
 
-export interface PlanCardProps {
+export interface PlanCardProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string;
   description: string;
   price: ReactNode | string;
@@ -11,31 +11,41 @@ export interface PlanCardProps {
   callToAction: ReactNode;
 }
 
-export function PlanCard(props: PlanCardProps): ReactElement {
+export function PlanCard({
+  name,
+  description,
+  price,
+  features,
+  adjustable,
+  highlighted,
+  callToAction,
+  className,
+  ...rest
+}: PlanCardProps): ReactElement {
   return (
     <article
       className={cn(
         'relative isolate rounded-3xl bg-white p-4 shadow-[inset_0_0_0_1px_theme(colors.green.400)] sm:p-8',
         'before:absolute before:inset-0 before:-z-10 before:rounded-3xl before:bg-[linear-gradient(#fff,#fff),linear-gradient(to_bottom,#E1FF00,#DEDACF,#68A8B6)] before:p-[4px] before:opacity-0 before:transition-[opacity] before:duration-500 before:content-[""] before:[background-clip:content-box,padding-box]',
-        props.highlighted && 'before:opacity-100',
+        highlighted && 'before:opacity-100',
+        className,
       )}
+      {...rest}
     >
       <header className="text-green-800">
         <div className="flex flex-row items-center gap-2">
-          <h2 className="text-2xl font-medium">{props.name}</h2>
-          {props.adjustable && (
+          <h2 className="text-2xl font-medium">{name}</h2>
+          {adjustable && (
             <span className="whitespace-nowrap rounded-full bg-green-100 px-3 py-1 text-sm font-medium leading-5">
               Adjust your plan at any time
             </span>
           )}
         </div>
-        <p className="mt-2">{props.description}</p>
+        <p className="mt-2">{description}</p>
       </header>
-      <div className="mt-4 text-5xl font-medium leading-[56px] tracking-[-0.48px]">
-        {props.price}
-      </div>
-      <div className="mt-4 flex *:grow">{props.callToAction}</div>
-      <ul className="mt-4 text-green-800">{props.features}</ul>
+      <div className="mt-4 text-5xl font-medium leading-[56px] tracking-[-0.48px]">{price}</div>
+      <div className="mt-4 flex *:grow">{callToAction}</div>
+      <ul className="mt-4 text-green-800">{features}</ul>
     </article>
   );
 }
