073b518ee01511268861493747db0e0470cc1f58
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-02-27 20:21:30 +0100

    Improve pricing tooltip arrow

--- packages/web/docs/src/components/pricing/index.tsx
+++ packages/web/docs/src/components/pricing/index.tsx
@@ -33,11 +33,21 @@ function Tooltip({ content, children }: { content: string; children: ReactNode }
         {children}
       </Trigger>
       <Content
+        align="start"
         sideOffset={5}
         className="bg-green-1000 z-20 rounded p-2 text-sm font-normal leading-4 text-white shadow"
       >
         {content}
-        <Arrow className="fill-green-1000" width={9} />
+        <svg
+          // radix arrow is in wrong spot, so I added a custom one
+          width="10"
+          height="14"
+          viewBox="0 0 12 16"
+          fill="currentColor"
+          className="text-green-1000 absolute bottom-0 left-1/3 -translate-x-1/2 translate-y-1/2"
+        >
+          <path d="M0 8L6 0L12 8L6 16L0 8Z" />
+        </svg>
       </Content>
     </Root>
   );
