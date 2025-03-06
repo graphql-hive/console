fdafc38933b691b853d3fa393bf4027fbb0f4f28
jdolle <1841898+jdolle@users.noreply.github.com>
2025-03-06 12:02:13 -0800

    Remove extra comma

--- packages/services/api/src/modules/schema/providers/schema-publisher.ts
+++ packages/services/api/src/modules/schema/providers/schema-publisher.ts
@@ -345,7 +345,7 @@ export class SchemaPublisher {
           errors: [
             {
               message:
-                'Invalid service name. Service name must be 64 characters or less, must start with a letter, and can only contain alphanumeric characters, dash (-), underscore (_), , or forward slash (/).',
+                'Invalid service name. Service name must be 64 characters or less, must start with a letter, and can only contain alphanumeric characters, dash (-), underscore (_), or forward slash (/).',
             },
           ],
         };
@@ -1090,7 +1090,7 @@ export class SchemaPublisher {
           errors: [
             {
               message:
-                'Invalid service name. Service name must be 64 characters or less, must start with a letter, and can only contain alphanumeric characters, dash (-), underscore (_), , or forward slash (/).',
+                'Invalid service name. Service name must be 64 characters or less, must start with a letter, and can only contain alphanumeric characters, dash (-), underscore (_), or forward slash (/).',
             },
           ],
         };
