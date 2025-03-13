d3b3d3dcee87d9e1dd62986e1b5045d4d3612f28
jdolle <1841898+jdolle@users.noreply.github.com>
2025-03-13 11:29:20 -0700

    More intuitive selection and clipboard support

--- packages/web/app/src/lib/hooks/use-timed.ts
+++ packages/web/app/src/lib/hooks/use-timed.ts
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useTimed(wait: number = 1000) {
   const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  useEffect(() => {
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [timer]);
+
   const handler = () => {
     if (timer) {
       clearTimeout(timer);
