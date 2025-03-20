58ef9ffcb017d2815b7b19edf2b232e2af7d8fa2
jdolle <1841898+jdolle@users.noreply.github.com>
2025-03-20 09:38:53 -0700

    Use idTranslator's resolveTargetReference

--- packages/services/api/src/modules/target/resolvers/Mutation/updateTargetDangerousChangeClassification.ts
+++ packages/services/api/src/modules/target/resolvers/Mutation/updateTargetDangerousChangeClassification.ts
@@ -4,25 +4,16 @@ import { MutationResolvers } from './../../../../__generated__/types';
 
 export const updateTargetDangerousChangeClassification: NonNullable<
   MutationResolvers['updateTargetDangerousChangeClassification']
-> = async (_, { input: { failDangerousChecks, target: targetRef } }, { injector }) => {
-  const targetManager = injector.get(TargetManager);
-  let targetId: string, projectId: string, organizationId: string;
-  if (targetRef.byId) {
-    const target = await targetManager.getTargetById({ targetId: targetRef.byId });
-    targetId = target.id;
-    projectId = target.projectId;
-    organizationId = target.orgId;
-  } else if (targetRef.bySelector) {
-    const translator = injector.get(IdTranslator);
-    [organizationId, projectId, targetId] = await Promise.all([
-      translator.translateOrganizationId(targetRef.bySelector),
-      translator.translateProjectId(targetRef.bySelector),
-      translator.translateTargetId(targetRef.bySelector),
-    ]);
-  } else {
-    throw new Error();
+> = async (_, { input: { failDangerousChecks, target } }, { injector, session }) => {
+  const translator = injector.get(IdTranslator);
+  const selector = await translator.resolveTargetReference({ reference: target });
+
+  if (!selector) {
+    session.raise('target:modifySettings');
   }
 
+  const { targetId, projectId, organizationId } = selector as NonNullable<typeof selector>;
+  const targetManager = injector.get(TargetManager);
   await targetManager.updateTargetDangerousChangeClassification({
     failDangerousChecks,
     targetId,
