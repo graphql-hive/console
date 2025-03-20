1008c9bf2b370f7c22d37dcd7acfd902d57445ff
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-20 18:44:37 +0100

    Change latest-posts heading to designer preference

--- packages/web/docs/src/app/blog/components/posts-by-tag/latest-posts.tsx
+++ packages/web/docs/src/app/blog/components/posts-by-tag/latest-posts.tsx
@@ -1,28 +1,21 @@
 import { Heading } from '@theguild/components';
 import { BlogPostFile } from '../../blog-types';
 import { BlogCard } from '../blog-card';
-
-// import { prettyPrintTag } from '../pretty-print-tag';
+import { prettyPrintTag } from '../pretty-print-tag';
 
 export function LatestPosts({ posts, tag }: { posts: BlogPostFile[]; tag: string | null }) {
   return (
     <section className="sm:pt-12">
       <Heading size="md" as="h2" className="text-center">
         Latest posts
-        {/* Probably redundant, but I wanted to consult designers. */}
-        {/* {' '} */}
-        {/* {tag ? (
+        {tag ? (
           <>
-            in{' '}
-            <span
-            // className="text-green-1000 inline-block rounded-2xl bg-gradient-to-r from-blue-300 to-blue-500 px-3 py-2"
-            >
-              {prettyPrintTag(tag)}
-            </span>
+            {' '}
+            in <span>{prettyPrintTag(tag)}</span>
           </>
         ) : (
           ''
-        )} */}
+        )}
       </Heading>
       <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid sm:grid-cols-2 sm:gap-6 md:mt-16 lg:grid-cols-3 xl:grid-cols-4">
         {posts.map(post => {
