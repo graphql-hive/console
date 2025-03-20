2343fde9b5051ff2a7f50feee30893ec4ee2d847
Laurin Quast <laurinquast@googlemail.com>
2025-03-20 10:28:50 +0100

    ts

--- packages/services/api/src/modules/shared/resolvers/Query/_.ts
+++ packages/services/api/src/modules/shared/resolvers/Query/_.ts
@@ -1,5 +1,5 @@
 import type { QueryResolvers } from './../../../../__generated__/types';
 
 export const _: NonNullable<QueryResolvers['_']> = async (_parent, _arg, _ctx) => {
-  /* Implement Query._ resolver logic here */
+  return null;
 };
