5821415fb12789cb37d09a42555fce9f764bde2c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-24 23:04:21 +0100

    Render BlogPostPicture inside of header component to make styling simpler

--- packages/web/docs/src/app/blog/components/blog-post-layout/blog-post-header.tsx
+++ packages/web/docs/src/app/blog/components/blog-post-layout/blog-post-header.tsx
@@ -6,42 +6,50 @@ import { useFrontmatter } from '../../../../components/use-frontmatter';
 import { ProductUpdateAuthors } from '../../../product-updates/(posts)/product-update-header';
 import type { BlogFrontmatter } from '../../blog-types';
 import { BlogTagChip } from '../blog-tag-chip';
+import { BlogPostPicture } from './blog-post-picture';
 
 export function BlogPostHeader({ className }: { className?: string }) {
-  const { frontmatter } = useFrontmatter<BlogFrontmatter>();
+  const {
+    frontmatter: { image, tags, authors, title, date },
+  } = useFrontmatter<BlogFrontmatter>();
 
-  const tag = frontmatter.tags[0];
+  const tag = tags[0];
+
   return (
-    <header
-      className={cn(
-        'flex flex-col items-center rounded-3xl bg-[rgb(var(--nextra-bg))] px-12 pb-12 pt-6 xl:w-[888px]',
-        className,
-      )}
-    >
-      <div className="flex items-center gap-2">
-        <Anchor href="/blog" className="flex items-center gap-2 text-sm font-medium">
-          <ArrowIcon className="text-beige-1000 mr-1 size-4 rotate-180" />
-          <span className="text-beige-800">
-            Blog
-            {tag && <span> /</span>}
-          </span>
-        </Anchor>
-        {tag && <BlogTagChip tag={tag} colorScheme="default" />}
-      </div>
-      <Heading
-        as="h1"
-        size="md"
-        className="mb-0 mt-4 w-[--article-max-width] text-pretty text-center"
+    <>
+      {image && <BlogPostPicture image={image} />}
+      <header
+        className={cn(
+          'flex flex-col items-center rounded-3xl bg-[rgb(var(--nextra-bg))] px-1 pb-6 pt-4 md:px-12 md:pb-12 md:pt-6 xl:w-[888px]',
+          image && '-mt-20 max-sm:mx-6',
+          className,
+        )}
       >
-        {frontmatter.title}
-      </Heading>
-      <ProductUpdateAuthors
-        meta={{
-          authors: Array.isArray(frontmatter.authors) ? frontmatter.authors : [frontmatter.authors],
-          date: frontmatter.date,
-        }}
-        className="mt-4"
-      />
-    </header>
+        <div className="flex items-center gap-2">
+          <Anchor href="/blog" className="flex items-center gap-2 text-sm font-medium">
+            <ArrowIcon className="text-beige-1000 mr-1 size-4 rotate-180" />
+            <span className="text-beige-800">
+              Blog
+              {tag && <span> /</span>}
+            </span>
+          </Anchor>
+          {tag && <BlogTagChip tag={tag} colorScheme="default" />}
+        </div>
+        <Heading
+          as="h1"
+          size="md"
+          className="mb-0 mt-4 w-[--article-max-width] text-pretty text-center"
+        >
+          {title}
+        </Heading>
+        <ProductUpdateAuthors
+          meta={{
+            authors: Array.isArray(authors) ? authors : [authors],
+            date,
+          }}
+          className="mt-4"
+        />
+      </header>
+    </>
   );
 }
--- packages/web/docs/src/app/blog/components/blog-post-layout/blog-post-picture.tsx
+++ packages/web/docs/src/app/blog/components/blog-post-layout/blog-post-picture.tsx
@@ -1,19 +1,14 @@
-'use client';
-
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { cn } from '@theguild/components';
-import { useFrontmatter } from '../../../../components/use-frontmatter';
-import type { BlogFrontmatter } from '../../blog-types';
 
-export function BlogPostPicture({ className }: { className?: string }) {
-  const { frontmatter } = useFrontmatter<BlogFrontmatter>();
-  const image = frontmatter.image;
-
-  if (!image) {
-    return null;
-  }
-
-  className = cn('h-[324px] rounded-3xl overflow-hidden object-cover [&+*]:-mt-20', className);
+export function BlogPostPicture({
+  className,
+  image,
+}: {
+  className?: string;
+  image: string | StaticImageData;
+}) {
+  className = cn('h-[324px] rounded-3xl overflow-hidden object-cover', className);
 
   if (typeof image === 'string' && (image.endsWith('.webm') || image.endsWith('.mp4'))) {
     return <video className={className} src={image} autoPlay muted loop playsInline />;
