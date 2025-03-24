cc6b013099f50db1e2d8f171eb8cc69a415e741d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-24 21:14:48 +0100

    Refactor MDX types out of Case Study files

---
+++ packages/web/docs/src/mdx-types.ts
@@ -0,0 +1,16 @@
+import type { getPageMap } from '@theguild/components/server';
+
+/**
+ * TODO: This type should be exported from `nextra` and `@theguild/components`
+ */
+export type MdxFile<FrontMatterType> = {
+  name: string;
+  route: string;
+  frontMatter?: FrontMatterType;
+};
+
+
+/**
+ * TODO: This should be exported from `nextra` and `@theguild/components`
+ */
+export type PageMapItem = Awaited<ReturnType<typeof getPageMap>>[number];
