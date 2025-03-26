e45b55d4094721e009129a4a130ed254ff9495be
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-26 18:27:31 +0100

    Add missing components and imports

--- packages/web/docs/src/components/youtube-iframe.tsx
+++ packages/web/docs/src/components/youtube-iframe.tsx
@@ -10,13 +10,16 @@ export function YoutubeIframe({
   className?: string;
 }) {
   return (
+    // I'm not sure if this is not a false positive, and it YouTube
+    // iframes don't work without both allow-same-origin and allow-scripts
+    // eslint-disable-next-line react/iframe-missing-sandbox
     <iframe
       className={cn('mt-6 h-[400px] w-full', className)}
       src={src}
       title={title}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-      sandbox="allow-scripts allow-presentation"
+      sandbox="allow-scripts allow-presentation allow-same-origin allow-popups"
     />
   );
 }
