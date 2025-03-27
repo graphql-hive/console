e50ebf63f13654d566d42aacb958c63b7fea1e27
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-27 13:50:25 +0100

    Make sure cards are same height on tablet sized screens

--- packages/web/docs/src/app/blog/components/posts-by-tag/latest-posts.tsx
+++ packages/web/docs/src/app/blog/components/posts-by-tag/latest-posts.tsx
@@ -20,7 +20,7 @@ export function LatestPosts({ posts, tag }: { posts: BlogPostFile[]; tag: string
       <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid sm:grid-cols-2 sm:gap-6 md:mt-16 lg:grid-cols-3 xl:grid-cols-4">
         {posts.map(post => {
           return (
-            <li key={post.route} className="basis-1/3">
+            <li key={post.route} className="*:h-full">
               <BlogCard post={post} tag={tag} />
             </li>
           );
