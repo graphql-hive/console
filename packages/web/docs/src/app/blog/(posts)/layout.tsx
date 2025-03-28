440d02964cc05d6701b01a4938b57075f984fb0f
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-28 15:10:15 +0100

    Bring the style of first blog paragraph closer to Lede

--- packages/web/docs/src/app/blog/(posts)/layout.tsx
+++ packages/web/docs/src/app/blog/(posts)/layout.tsx
@@ -13,7 +13,7 @@ export default function BlogPostLayout({ children }: { children: React.ReactNode
       <div
         className={cn(
           MAIN_CONTENT,
-          'mx-auto flex *:!pl-2 sm:*:!ml-auto sm:*:!pl-0 [&>div>:first-child]:hidden [&_main>p:first-of-type]:text-2xl/8',
+          'mx-auto flex *:!pl-2 sm:*:!ml-auto sm:*:!pl-0 [&>div>:first-child]:hidden [&_main>p:first-of-type]:text-xl/6 md:[&_main>p:first-of-type]:text-2xl/8',
         )}
       >
         {children}
