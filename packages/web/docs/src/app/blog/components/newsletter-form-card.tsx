11b8c9584003f31caab2413dc22603900f50ef94
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-04-01 23:32:07 +0200

    Style error and success in Newsletter form

--- packages/web/docs/src/app/blog/components/newsletter-form-card.tsx
+++ packages/web/docs/src/app/blog/components/newsletter-form-card.tsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { useRef, useState } from 'react';
 import { CallToAction, cn, Heading } from '@theguild/components';
 
 export function NewsletterFormCard(props: React.HTMLAttributes<HTMLElement>) {
+  type Idle = undefined;
+  type Pending = { status: 'pending'; message?: never };
+  type Success = { status: 'success'; message: string };
+  type Error = { status: 'error'; message: string };
+  type State = Idle | Pending | Success | Error;
+  const [state, setState] = useState<State>();
+
+  // we don't want to blink a message on retries when request is pending
+  const lastMessage = useRef<string>();
+  lastMessage.current = state?.message || lastMessage.current;
+
   return (
     <article
       {...props}
@@ -17,20 +31,83 @@ export function NewsletterFormCard(props: React.HTMLAttributes<HTMLElement>) {
         >
           Stay in the loop
         </Heading>
-        <p className="mt-4">
-          Get the latest insights and best practices on GraphQL API management delivered straight to
-          your inbox.
-        </p>
+        <div className="relative mt-4">
+          <p style={{ opacity: lastMessage.current ? 0 : 1 }}>
+            Get the latest insights and best practices on GraphQL API management delivered straight
+            to your inbox.
+          </p>
+          {lastMessage.current && <p className="absolute inset-0">{lastMessage.current}</p>}
+        </div>
       </div>
-      <form className="relative z-10 p-6">
-        {/* todo: add this to theguild/components */}
-        <input
-          placeholder="E-mail"
-          className="hive-focus hover:placeholder:text-green-1000/60 w-full rounded-lg border border-blue-400 bg-white py-3 indent-4 font-medium placeholder:text-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800/40 focus-visible:ring-0"
-        />
-        <CallToAction type="submit" variant="secondary-inverted" className="mt-2 !w-full">
-          Subscribe
-        </CallToAction>
+      <form
+        className="relative z-10 p-6"
+        onSubmit={async event => {
+          event.preventDefault();
+          const email = event.currentTarget.email.value;
+
+          if (!email?.includes('@')) {
+            setState({ status: 'error', message: 'Please enter a valid email address.' });
+            return;
+          }
+
+          setState({ status: 'pending' });
+
+          try {
+            const response = await fetch('https://utils.the-guild.dev/api/newsletter-subscribe', {
+              body: JSON.stringify({ email }),
+              method: 'POST',
+            });
+
+            setState((await response.json()) as { status: 'success' | 'error'; message: string });
+          } catch (e: unknown) {
+            if (!navigator.onLine) {
+              setState({
+                status: 'error',
+                message: 'Please check your internet connection and try again.',
+              });
+            }
+
+            if (e instanceof Error && e.message !== 'Failed to fetch') {
+              setState({ status: 'error', message: e.message });
+            }
+
+            setState({ status: 'error', message: 'Something went wrong. Please let us know.' });
+          }
+        }}
+      >
+        <Input name="email" placeholder="E-mail" error={state?.status === 'error'} />
+        {!state || state.status === 'error' ? (
+          <CallToAction type="submit" variant="secondary-inverted" className="mt-2 !w-full">
+            Subscribe
+          </CallToAction>
+        ) : state.status === 'pending' ? (
+          <CallToAction
+            type="submit"
+            variant="secondary-inverted"
+            className="mt-2 !w-full"
+            disabled
+          >
+            Subscribing...
+          </CallToAction>
+        ) : state.status === 'success' ? (
+          <CallToAction
+            type="reset"
+            variant="secondary-inverted"
+            className="group/button mt-2 !w-full before:absolute"
+            onClick={() => {
+              // the default behavior of <button type="reset"> doesn't work here
+              // because it gets unmounted too fast
+              setTimeout(() => {
+                setState(undefined);
+              }, 0);
+            }}
+          >
+            <span className="group-hover/button:hidden group-focus/button:hidden">Subscribed</span>
+            <span aria-hidden className="hidden group-hover/button:block group-focus/button:block">
+              Another email?
+            </span>
+          </CallToAction>
+        ) : null}
       </form>
       <DecorationArch color="#A2C1C4" className="absolute bottom-0 right-0" />
     </article>
@@ -58,5 +135,23 @@ function DecorationArch({ className, color }: { className?: string; color: strin
         </linearGradient>
       </defs>
     </svg>
+  );
+}
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
+
+function Input({ error, ...props }: InputProps) {
+  /* todo: add this to theguild/components */
+  return (
+    <input
+      {...props}
+      className={cn(
+        'hover:placeholder:text-green-1000/60 w-full rounded-lg border border-blue-400 bg-white py-3 indent-4 font-medium placeholder:text-green-800 autofill:shadow-[inset_0_0_0px_1000px_rgb(255,255,255)] autofill:first-line:font-sans focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800/40 focus-visible:ring-0',
+        props.className,
+        error && 'border-critical-dark/20 !outline-critical-dark',
+      )}
+    />
   );
 }
