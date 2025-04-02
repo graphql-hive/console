2df83b047a32caf515b4d99559614eb49693614d
Laurin Quast <laurinquast@googlemail.com>
2025-04-02 14:29:01 +0200

    sub

---
+++ packages/services/api/src/modules/shared/resolvers/Subscription/_.ts
@@ -0,0 +1,7 @@
+import type { SubscriptionResolvers } from './../../../../__generated__/types';
+
+export const _: NonNullable<SubscriptionResolvers['_']> = {
+  async *subscribe() {
+    return;
+  },
+};
