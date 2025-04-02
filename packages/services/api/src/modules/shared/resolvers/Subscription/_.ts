6cd92707de19d3986ce31165a11a22074cdd62be
Laurin Quast <laurinquast@googlemail.com>
2025-04-02 14:55:36 +0200

    ...

--- packages/services/api/src/modules/shared/resolvers/Subscription/_.ts
+++ packages/services/api/src/modules/shared/resolvers/Subscription/_.ts
@@ -1,6 +1,7 @@
 import type { SubscriptionResolvers } from './../../../../__generated__/types';
 
 export const _: NonNullable<SubscriptionResolvers['_']> = {
+  // eslint-disable-next-line require-yield
   async *subscribe() {
     return;
   },
