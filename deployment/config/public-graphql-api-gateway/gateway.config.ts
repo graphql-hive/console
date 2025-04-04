7a718e4ca33a09948256e8c51a9413ac779177d7
Laurin Quast <laurinquast@googlemail.com>
2025-04-04 12:14:28 +0200

    forward headers etc

--- deployment/config/public-graphql-api-gateway/gateway.config.ts
+++ deployment/config/public-graphql-api-gateway/gateway.config.ts
@@ -1,6 +1,11 @@
 // @ts-expect-error not a dependency
 import { defineConfig } from '@graphql-hive/gateway';
 
+const defaultQuery = `#
+# Welcome to the Hive Console GraphQL API.
+#
+`;
+
 export const gatewayConfig = defineConfig({
   transportEntries: {
     graphql: {
@@ -12,4 +17,16 @@ export const gatewayConfig = defineConfig({
     endpoint: process.env['SUPERGRAPH_ENDPOINT'],
     key: process.env['HIVE_CDN_ACCESS_TOKEN'],
   },
+  graphiql: {
+    title: 'Hive Console - GraphQL API',
+    defaultQuery,
+  },
+  propagateHeaders: {
+    fromClientToSubgraphs({ request }) {
+      return {
+        'x-request-id': request.headers.get('x-request-id'),
+      };
+    },
+  },
+  disableWebsockets: true,
 });
