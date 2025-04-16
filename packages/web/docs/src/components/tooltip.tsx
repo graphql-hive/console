fa48638957756ec73edcd083a6b694af401999fb
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-04-16 15:11:22 +0200

    Make the Pricing tooltip a bit larger after chatting with designers

--- packages/web/docs/src/components/tooltip.tsx
+++ packages/web/docs/src/components/tooltip.tsx
@@ -10,20 +10,19 @@ export function Tooltip({ content, children }: { content: string; children: Reac
       <Content
         align="start"
         sideOffset={5}
-        className="bg-green-1000 z-20 rounded px-2 py-0.5 text-sm font-normal leading-4 text-white shadow"
+        className="bg-green-1000 z-20 rounded px-2 py-[3px] text-sm font-normal text-white shadow"
       >
         {content}
-        <svg
-          // radix arrow is in wrong spot, so I added a custom one
-          width="10"
-          height="14"
-          viewBox="0 0 12 16"
-          fill="currentColor"
-          className="text-green-1000 absolute bottom-0 left-1/3 -translate-x-1/2 translate-y-1/2"
-        >
-          <path d="M0 8L6 0L12 8L6 16L0 8Z" />
-        </svg>
+        <TooltipArrow className="text-green-1000 absolute bottom-0 left-1/3 -translate-x-1/2 translate-y-full" />
       </Content>
     </Root>
+  );
+}
+
+function TooltipArrow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="10" height="5" viewBox="0 0 8 4" fill="currentColor" {...props}>
+      <path d="M5.06066 2.93934C4.47487 3.52513 3.52513 3.52513 2.93934 2.93934L-6.03983e-07 -6.99382e-07L8 0L5.06066 2.93934Z" />
+    </svg>
   );
 }
