65fe78d595bcd83acb6c212440fd536e28790008
Laurin Quast <laurinquast@googlemail.com>
2025-05-20 14:45:19 +0200

    wip: public api docs

---
+++ packages/web/docs/src/content/graphql-api/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  index: 'Using the GraphQL API',
+  'member-management': 'Manage Members',
+  'access-token-management': 'Manage Access Tokens',
+  'project-management': 'Manage Projects',
+};
