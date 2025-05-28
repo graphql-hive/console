5914d650e91281a392d4ebdb73fc7abb1ed22b1c
Laurin Quast <laurinquast@googlemail.com>
2025-05-28 16:02:37 +0200

    sort columns and indices

--- packages/migrations/src/clickhouse-actions/015-otel-trace.ts
+++ packages/migrations/src/clickhouse-actions/015-otel-trace.ts
@@ -92,28 +92,32 @@ export const action: Action = async exec => {
       , "trace_id" String CODEC(ZSTD(1))
       , "span_id" String CODEC(ZSTD(1))
       , "timestamp" DateTime('UTC') CODEC(DoubleDelta, LZ4)
-      , "operation_name" String CODEC(ZSTD(1))
-      , "operation_type" LowCardinality(String) CODEC(ZSTD(1))
-      , "operation_body" String CODEC(ZSTD(1))
       , "duration" UInt64 CODEC(T64, ZSTD(1))
-      , "subgraph_names" Array(LowCardinality(String)) CODEC(ZSTD(1))
       , "http_status_code" String CODEC(ZSTD(1))
       , "http_method" String CODEC(ZSTD(1))
       , "http_host" String CODEC(ZSTD(1))
       , "http_route" String CODEC(ZSTD(1))
       , "http_url" String CODEC(ZSTD(1))
-      , INDEX "idx_operation_name" "operation_name" TYPE bloom_filter(0.01) GRANULARITY 1
-      , INDEX "idx_operation_type" "operation_type" TYPE bloom_filter(0.01) GRANULARITY 1
+      , "client_name" String Codec(ZSTD(1))
+      , "client_version" String Codec(ZSTD(1))
+      , "graphql_operation_name" String CODEC(ZSTD(1))
+      , "graphql_operation_type" LowCardinality(String) CODEC(ZSTD(1))
+      , "graphql_operation_document" String CODEC(ZSTD(1))
+      , "subgraph_names" Array(LowCardinality(String)) CODEC(ZSTD(1))
       , INDEX "idx_duration" "duration" TYPE minmax GRANULARITY 1
-      , INDEX "idx_subgraph_names" "subgraph_names" TYPE bloom_filter(0.01) GRANULARITY 1
       , INDEX "idx_http_status_code" "http_status_code" TYPE bloom_filter(0.01) GRANULARITY 1
       , INDEX "idx_http_method" "http_method" TYPE bloom_filter(0.01) GRANULARITY 1
       , INDEX "idx_http_host" "http_host" TYPE bloom_filter(0.01) GRANULARITY 1
       , INDEX "idx_http_route" "http_route" TYPE bloom_filter(0.01) GRANULARITY 1
       , INDEX "idx_http_url" "http_url" TYPE bloom_filter(0.01) GRANULARITY 1
+      , INDEX "idx_client_name" "client_name" TYPE bloom_filter(0.01) GRANULARITY 1
+      , INDEX "idx_client_version" "client_version" TYPE bloom_filter(0.01) GRANULARITY 1
+      , INDEX "idx_graphql_operation_name" "graphql_operation_name" TYPE bloom_filter(0.01) GRANULARITY 1
+      , INDEX "idx_graphql_operation_type" "graphql_operation_type" TYPE bloom_filter(0.01) GRANULARITY 1
+      , INDEX "idx_subgraph_names" "subgraph_names" TYPE bloom_filter(0.01) GRANULARITY 1
     )
     ENGINE = MergeTree
-    PARTITION BY toDate(timestamp)
+    PARTITION BY toDate("timestamp")
     ORDER BY ("target_id", "timestamp")
     TTL toDateTime(timestamp) + toIntervalDay(365)
     SETTINGS
@@ -123,37 +127,41 @@ export const action: Action = async exec => {
 
   await exec(`
     CREATE MATERIALIZED VIEW IF NOT EXISTS "otel_traces_normalized_mv" TO "otel_traces_normalized" (
-      "trace_id" String
+      , "target_id" LowCardinality(String)
+      , "trace_id" String
       , "span_id" String
       , "timestamp" DateTime('UTC')
-      , "operation_name" String
-      , "operation_type" LowCardinality(String)
-      , "target_id" LowCardinality(String)
       , "duration" UInt64
-      , "operation_body" String
-      , "subgraph_names" Array(String)
       , "http_status_code" String
       , "http_host" String
       , "http_method" String
       , "http_route" String
       , "http_url" String
+      , "client_name" String
+      , "client_version" String
+      , "graphql_operation_name" String
+      , "graphql_operation_type" LowCardinality(String)
+      , "graphql_operation_document" String
+      , "subgraph_names" Array(String)
     )
     AS (
       SELECT
-        "TraceId" as "trace_id"
+        toLowCardinality("SpanAttributes"['hive.target_id']) AS "target_id"
+        , "TraceId" as "trace_id"
         , "SpanId" AS "span_id"
         , toDateTime("Timestamp", 'UTC') AS "timestamp"
-        , "SpanAttributes"['graphql.operation.name'] AS operation_name
-        , toLowCardinality("SpanAttributes"['graphql.operation.type']) AS "operation_type"
-        , toLowCardinality("SpanAttributes"['hive.target_id']) AS "target_id"
         , "Duration" AS "duration"
-        , "SpanAttributes"['graphql.operation.document'] AS "operation_body"
-        , arrayMap(x -> toLowCardinality(x), splitByChar(',', "SpanAttributes"['subgraph.names'])) AS "subgraph_names"
         , "SpanAttributes"['http.status_code'] AS "http_status_code"
         , "SpanAttributes"['http.host'] AS "http_host"
         , "SpanAttributes"['http.method'] AS "http_method"
         , "SpanAttributes"['http.route'] AS "http_route"
         , "SpanAttributes"['http.url'] AS "http_url"
+        , "SpanAttributes"['hive.client.name'] AS "client_name"
+        , "SpanAttributes"['hive.client.version'] AS "client_version"
+        , "SpanAttributes"['graphql.operation.name'] AS "graphql_operation_name"
+        , toLowCardinality("SpanAttributes"['graphql.operation.type']) AS "graphql_operation_type"
+        , "SpanAttributes"['graphql.operation.document'] AS "graphql_operation_document"
+        , arrayMap(x -> toLowCardinality(x), splitByChar(',', "SpanAttributes"['hive.subgraph.names'])) AS "subgraph_names"
       FROM
         "otel_traces"
       WHERE
