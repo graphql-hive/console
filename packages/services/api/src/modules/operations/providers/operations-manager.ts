8eb9e144b7e9a452b2d596776d75d136540207ff
Laurin Quast <laurinquast@googlemail.com>
2025-10-06 16:57:20 +0200

    fix: oom for deprecated schema explorer (#7074)

--- packages/services/api/src/modules/operations/providers/operations-manager.ts
+++ packages/services/api/src/modules/operations/providers/operations-manager.ts
@@ -907,15 +907,6 @@ export class OperationsManager {
       typename: string;
     } & TargetSelector,
   ) {
-    await this.session.assertPerformAction({
-      action: 'project:describe',
-      organizationId: args.organizationId,
-      params: {
-        organizationId: args.organizationId,
-        projectId: args.projectId,
-      },
-    });
-
     const loader = this.getClientNamesPerCoordinateOfTypeLoader({
       target: args.targetId,
       period: args.period,
@@ -1118,6 +1109,14 @@ export class OperationsManager {
   }: {
     period: DateRange;
   } & TargetSelector) {
+    this.logger.debug(
+      'Count coordinates of target. (organizationId=%s, projectId=%s, targetId=%s, period=%o)',
+      organization,
+      project,
+      target,
+      period,
+    );
+
     await this.session.assertPerformAction({
       action: 'project:describe',
       organizationId: organization,
@@ -1131,6 +1130,15 @@ export class OperationsManager {
       target,
       period,
     });
+
+    this.logger.debug(
+      '%d coordinates found. (organizationId=%s, projectId=%s, targetId=%s, period=%o)',
+      rows.length,
+      organization,
+      project,
+      target,
+      period,
+    );
 
     const records: {
       [coordinate: string]: {
--- packages/services/api/src/modules/schema/resolvers/DeprecatedSchemaExplorer.ts
+++ packages/services/api/src/modules/schema/resolvers/DeprecatedSchemaExplorer.ts
@@ -1,26 +1,30 @@
 import { OperationsManager } from '../../operations/providers/operations-manager';
-import { buildGraphQLTypesFromSDL, withUsedByClients } from '../utils';
+import { buildGraphQLTypesFromSDL, memo, withUsedByClients } from '../utils';
 import type { DeprecatedSchemaExplorerResolvers } from './../../../__generated__/types';
 
 export const DeprecatedSchemaExplorer: DeprecatedSchemaExplorerResolvers = {
   types: ({ sdl, supergraph, usage }, _, { injector }) => {
     const operationsManager = injector.get(OperationsManager);
 
-    async function getStats(typename: string) {
-      const stats = await operationsManager.countCoordinatesOfTarget({
-        targetId: usage.targetId,
-        organizationId: usage.organizationId,
-        projectId: usage.projectId,
-        period: usage.period,
-      });
+    // NOTE: this function is super heavy.
+    const getStats = memo(
+      async function _getStats(typename: string) {
+        const stats = await operationsManager.countCoordinatesOfTarget({
+          targetId: usage.targetId,
+          organizationId: usage.organizationId,
+          projectId: usage.projectId,
+          period: usage.period,
+        });
 
-      return withUsedByClients(stats, {
-        selector: usage,
-        period: usage.period,
-        operationsManager,
-        typename,
-      });
-    }
+        return withUsedByClients(stats, {
+          selector: usage,
+          period: usage.period,
+          operationsManager,
+          typename,
+        });
+      },
+      arg => arg,
+    );
 
     return buildGraphQLTypesFromSDL(sdl, getStats, supergraph).sort((a, b) =>
       a.entity.name.localeCompare(b.entity.name),
