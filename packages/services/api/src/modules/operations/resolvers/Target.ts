676ccac69e65c366b975f379810f4c1eb0b9f410
Laurin Quast <laurinquast@googlemail.com>
2025-10-10 10:15:52 +0200

    i should comit this too :)

--- packages/services/api/src/modules/operations/resolvers/Target.ts
+++ packages/services/api/src/modules/operations/resolvers/Target.ts
@@ -1,9 +1,5 @@
-import Dataloader from 'dataloader';
-import stableJSONStringify from 'fast-json-stable-stringify';
 import { parseDateRangeInput } from '../../../shared/helpers';
-import { ClickHouse, sql } from '../providers/clickhouse-client';
 import { OperationsManager } from '../providers/operations-manager';
-import { SqlValue } from '../providers/sql';
 import { Traces } from '../providers/traces';
 import type { TargetResolvers } from './../../../__generated__/types';
 
@@ -109,136 +105,8 @@ export const Target: Pick<
       after ?? null,
     );
   },
-  tracesFilterOptions: async (target, { filter }, { injector }) => {
-    const ANDs: SqlValue[] = [sql`target_id = ${target.id}`];
-
-    if (filter?.traceIds?.length) {
-      ANDs.push(sql`"trace_id" IN (${sql.array(filter.traceIds, 'String')})`);
-    }
-
-    if (filter?.success?.length) {
-      const hasSuccess = filter.success.includes(true);
-      const hasError = filter.success.includes(false);
-
-      if (hasSuccess && !hasError) {
-        ANDs.push(
-          sql`"graphql_error_count" = 0`,
-          sql`substring("http_status_code", 1, 1) IN (${sql.array(['2', '3'], 'String')})`,
-        );
-      } else if (hasError && !hasSuccess) {
-        ANDs.push(
-          sql`"graphql_error_count" > 0`,
-          sql`substring("http_status_code", 1, 1) NOT IN (${sql.array(['2', '3'], 'String')})`,
-        );
-      }
-    }
-
-    if (filter?.operationNames?.length) {
-      ANDs.push(sql`"graphql_operation_name" IN (${sql.array(filter.operationNames, 'String')})`);
-    }
-
-    if (filter?.operationTypes?.length) {
-      ANDs.push(
-        sql`"graphql_operation_type" IN (${sql.array(
-          filter.operationTypes.map(value => (value == null ? '' : value.toLowerCase())),
-          'String',
-        )})`,
-      );
-    }
-
-    if (filter?.clientNames?.length) {
-      ANDs.push(sql`"client_name" IN (${sql.array(filter.clientNames, 'String')})`);
-    }
-
-    if (filter?.subgraphNames?.length) {
-      ANDs.push(
-        sql`hasAny("subgraph_names", (${sql.array(filter.subgraphNames.flat(), 'String')}))`,
-      );
-    }
-
-    if (filter?.httpStatusCodes?.length) {
-      ANDs.push(
-        sql`"http_status_code" IN (${sql.array(filter.httpStatusCodes.map(String), 'UInt16')})`,
-      );
-    }
-
-    if (filter?.httpMethods?.length) {
-      ANDs.push(sql`"http_method" IN (${sql.array(filter.httpMethods, 'String')})`);
-    }
-
-    if (filter?.httpHosts?.length) {
-      ANDs.push(sql`"http_host" IN (${sql.array(filter.httpHosts, 'String')})`);
-    }
-
-    if (filter?.httpRoutes?.length) {
-      ANDs.push(sql`"http_route" IN (${sql.array(filter.httpRoutes, 'String')})`);
-    }
-
-    if (filter?.httpUrls?.length) {
-      ANDs.push(sql`"http_url" IN (${sql.array(filter.httpUrls, 'String')})`);
-    }
-
-    const loader = new Dataloader<
-      {
-        key: string;
-        columnExpression: string;
-        limit: number | null;
-        arrayJoinColumn: string | null;
-      },
-      { value: string; count: number }[],
-      string
-    >(
-      async inputs => {
-        const statements: SqlValue[] = [];
-
-        for (const { key, columnExpression, limit, arrayJoinColumn } of inputs) {
-          statements.push(sql`
-            SELECT
-              ${key} AS "key",
-              toString(${sql.raw(columnExpression)}) AS "value",
-              count(*) AS "count"
-            FROM "otel_traces_normalized"
-            ${sql.raw(arrayJoinColumn ? `ARRAY JOIN ${arrayJoinColumn} AS "value"` : '')}
-            WHERE ${sql.join(ANDs, ' AND ')}
-            GROUP BY value
-            ORDER BY count DESC
-            ${sql.raw(limit ? `LIMIT ${limit}` : '')}
-          `);
-        }
-
-        const results = await injector.get(ClickHouse).query<{
-          key: string;
-          value: string;
-          count: number;
-        }>({
-          query: sql`
-            ${sql.join(statements, ' UNION ALL ')}
-          `,
-          queryId: 'traces_filter_options',
-          timeout: 10_000,
-        });
-
-        const rowsGroupedByKey = results.data.reduce(
-          (acc, row) => {
-            if (!acc[row.key]) {
-              acc[row.key] = [];
-            }
-            acc[row.key].push({ value: row.value, count: row.count });
-            return acc;
-          },
-          {} as Record<string, { value: string; count: number }[]>,
-        );
-
-        return inputs.map(input => rowsGroupedByKey[input.key] ?? []);
-      },
-      {
-        cacheKeyFn: stableJSONStringify,
-      },
-    );
-
-    return {
-      loader,
-    };
+  tracesFilterOptions(target, { filter }, { injector }) {
+    return injector.get(Traces).getTraceFilterOptions(target.id, filter ?? null);
   },
   trace(target, args, { injector }) {
     return injector.get(Traces).findTraceById(target.orgId, target.id, args.traceId);
