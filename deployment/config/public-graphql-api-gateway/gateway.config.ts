fc0351eb9c28cfdd18c47c46c4d6dcb67d301179
Laurin Quast <laurinquast@googlemail.com>
2025-10-14 14:48:20 +0200

    inline

--- deployment/config/public-graphql-api-gateway/gateway.config.ts
+++ deployment/config/public-graphql-api-gateway/gateway.config.ts
@@ -2,23 +2,81 @@
 import { defineConfig } from '@graphql-hive/gateway';
 // @ts-expect-error not a dependency
 import { hiveTracingSetup } from '@graphql-hive/plugin-opentelemetry/setup';
+import type { Context } from '@opentelemetry/api';
 import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import { globalErrorHandler } from '@opentelemetry/core';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { MultiSpanProcessor } from '@opentelemetry/sdk-trace-base/build/src/MultiSpanProcessor';
+import {
+  BatchSpanProcessor,
+  SpanProcessor,
+  type ReadableSpan,
+  type Span,
+} from '@opentelemetry/sdk-trace-base';
 
-if (process.env['OPENTELEMETRY_COLLECTOR_ENDPOINT'] || process.env['HIVE_TRACING_ACCESS_TOKEN']) {
+/** Note: this is inlined for now... */
+class MultiSpanProcessor implements SpanProcessor {
+  constructor(private readonly _spanProcessors: SpanProcessor[]) {}
+
+  forceFlush(): Promise<void> {
+    const promises: Promise<void>[] = [];
+
+    for (const spanProcessor of this._spanProcessors) {
+      promises.push(spanProcessor.forceFlush());
+    }
+    return new Promise(resolve => {
+      Promise.all(promises)
+        .then(() => {
+          resolve();
+        })
+        .catch(error => {
+          globalErrorHandler(error || new Error('MultiSpanProcessor: forceFlush failed'));
+          resolve();
+        });
+    });
+  }
+
+  onStart(span: Span, context: Context): void {
+    for (const spanProcessor of this._spanProcessors) {
+      spanProcessor.onStart(span, context);
+    }
+  }
+
+  onEnd(span: ReadableSpan): void {
+    for (const spanProcessor of this._spanProcessors) {
+      spanProcessor.onEnd(span);
+    }
+  }
+
+  shutdown(): Promise<void> {
+    const promises: Promise<void>[] = [];
+
+    for (const spanProcessor of this._spanProcessors) {
+      promises.push(spanProcessor.shutdown());
+    }
+    return new Promise((resolve, reject) => {
+      Promise.all(promises).then(() => {
+        resolve();
+      }, reject);
+    });
+  }
+}
+
+if (process.env['OPENTELEMETRY_COLLECTOR_ENDPOINT'] || process.env['HIVE_TRACE_ACCESS_TOKEN']) {
   hiveTracingSetup({
+    // Noop is only there to not raise an exception in case we do not hive console tracing.
+    target: process.env['HIVE_TARGET'] ?? 'noop',
     contextManager: new AsyncLocalStorageContextManager(),
     processor: new MultiSpanProcessor([
-      ...(process.env['HIVE_TRACING_ACCESS_TOKEN']
+      ...(process.env['HIVE_TRACE_ACCESS_TOKEN'] &&
+      process.env['HIVE_TRACING_ENDPOINT'] &&
+      process.env['HIVE_TARGET']
         ? [
             new BatchSpanProcessor(
               new OTLPTraceExporter({
                 url: process.env['HIVE_TRACING_ENDPOINT'],
                 headers: {
-                  Authorization: `Bearer ${process.env['HIVE_TRACING_ACCESS_TOKEN']}`,
-                  'X-Hive-Target-Ref': process.env.HIVE_TRACING_TARGET!,
+                  Authorization: `Bearer ${process.env['HIVE_TRACE_ACCESS_TOKEN']}`,
+                  'X-Hive-Target-Ref': process.env['HIVE_TARGET'],
                 },
               }),
             ),
