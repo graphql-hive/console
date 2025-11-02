8be86446b42f100cbefe0963e6d9370162ad840d
Dotan Simha <dotansimha@gmail.com>
2025-11-02 09:24:39 +0200

    docs(web): updated footer link to use HTML format for privacy policy

--- packages/web/docs/src/app/layout.tsx
+++ packages/web/docs/src/app/layout.tsx
@@ -113,7 +113,7 @@ export default async function HiveDocsLayout({ children }: { children: ReactNode
             resources: [
               {
                 children: 'Privacy Policy',
-                href: 'https://the-guild.dev/graphql/hive/privacy-policy.pdf',
+                href: 'https://the-guild.dev/graphql/hive/privacy-policy.html',
                 title: 'Privacy Policy',
               },
               {
