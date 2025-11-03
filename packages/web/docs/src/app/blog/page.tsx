0656f2f3e4645e2faf2ae9e362a36ec1ae93778d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-11-03 13:48:45 +0100

    Show case studies and product updates in blog

--- packages/web/docs/src/app/blog/page.tsx
+++ packages/web/docs/src/app/blog/page.tsx
@@ -1,4 +1,6 @@
 import { getPageMap } from '@theguild/components/server';
+import { coerceCaseStudiesToBlogs } from '../case-studies/coerce-case-studies-to-blogs';
+import { getCaseStudies } from '../case-studies/get-case-studies';
 import { isBlogPost } from './blog-types';
 import { NewsletterFormCard } from './components/newsletter-form-card';
 import { PostsByTag } from './components/posts-by-tag';
@@ -13,7 +15,18 @@ export const metadata = {
 
 export default async function BlogPage() {
   const [_meta, _indexPage, ...pageMap] = await getPageMap('/blog');
-  const allPosts = pageMap.filter(isBlogPost);
+  const [, , ...productUpdates] = await getPageMap('/product-updates');
+
+  const caseStudies = await getCaseStudies().then(coerceCaseStudiesToBlogs);
+
+  const allPosts = pageMap
+    .filter(isBlogPost)
+    .concat(caseStudies)
+    .concat(
+      productUpdates
+        .filter(isBlogPost)
+        .map(post => ((post.frontMatter.tags ||= ['Product Update']), post)),
+    );
 
   return (
     <BlogPageLayout>
---
+++ packages/web/docs/src/app/case-studies/get-case-studies.ts
@@ -0,0 +1,14 @@
+import { getPageMap } from '@theguild/components/server';
+import { isCaseStudy } from './isCaseStudyFile';
+
+export async function getCaseStudies() {
+  const [_meta, _indexPage, ...pageMap] = await getPageMap('/case-studies');
+
+  const caseStudies = pageMap.filter(isCaseStudy).sort((a, b) => {
+    const aDate = a.frontMatter.date;
+    const bDate = b.frontMatter.date;
+    return aDate < bDate ? 1 : aDate > bDate ? -1 : 0;
+  });
+
+  return caseStudies;
+}
