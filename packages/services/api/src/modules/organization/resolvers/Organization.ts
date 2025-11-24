041045e31cae8746a305a15046eb7157faae7662
jdolle <1841898+jdolle@users.noreply.github.com>
2025-11-24 08:13:57 -0800

    Update packages/services/api/src/modules/organization/resolvers/Organization.ts
    Co-authored-by: Laurin Quast <laurinquast@googlemail.com>

--- packages/services/api/src/modules/organization/resolvers/Organization.ts
+++ packages/services/api/src/modules/organization/resolvers/Organization.ts
@@ -67,7 +67,7 @@ export const Organization: Pick<
       .getPaginatedOrganizationMembersForOrganization(organization, {
         first: args.first ?? null,
         after: args.after ?? null,
-        searchTerm: args.filters?.searchTerm,
+        searchTerm: args.filters?.searchTerm ?? null,
       });
   },
   invitations: async (organization, args, { injector }) => {
