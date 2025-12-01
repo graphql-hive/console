e5bef2e3c54015d18323cc5bc813f1c868daef1c
Laurin Quast <laurinquast@googlemail.com>
2025-12-01 17:54:41 +0100

    better

--- integration-tests/tests/api/cors.spec.ts
+++ integration-tests/tests/api/cors.spec.ts
@@ -5,29 +5,36 @@ const registryAddress = await getServiceHost('server', 8082);
 
 const endpoint = `http://${registryAddress}`;
 
-test('no origin -> no cors', async () => {
+test('no origin header -> no CORS checks', async () => {
   const request = await fetch(`${endpoint}/graphql`);
   expect(request.headers.get('access-control-allow-origin')).toEqual(null);
+  expect(request.headers.get('access-control-allow-credentials')).toEqual(null);
+  expect(request.status).toEqual(200);
+  expect(await request.text()).toMatchInlineSnapshot(
+    `{"errors":[{"message":"Must provide query string.","extensions":{"code":"BAD_REQUEST"}}]}`,
+  );
 });
 
-test('unmatching origin -> cors error', async () => {
+test('unmatching origin -> send CORS error', async () => {
   const request = await fetch(`${endpoint}/graphql`, {
     headers: {
       origin: 'https://evil.com',
     },
   });
   expect(request.headers.get('access-control-allow-origin')).toEqual(null);
+  expect(request.headers.get('access-control-allow-credentials')).toEqual(null);
   expect(request.status).toEqual(403);
   expect(await request.text()).toMatchInlineSnapshot(`CORS origin not allowed.`);
 });
 
-test('matching -> cors!', async () => {
+test('matching origin -> send correct CORS headers', async () => {
   const request = await fetch(`${endpoint}/graphql`, {
     headers: {
       origin: ensureEnv('HIVE_APP_BASE_URL'),
     },
   });
   expect(request.headers.get('access-control-allow-origin')).toEqual('http://localhost:3000');
+  expect(request.headers.get('access-control-allow-credentials')).toEqual('true');
   expect(request.status).toEqual(200);
   expect(await request.text()).toMatchInlineSnapshot(
     `{"errors":[{"message":"Must provide query string.","extensions":{"code":"BAD_REQUEST"}}]}`,
