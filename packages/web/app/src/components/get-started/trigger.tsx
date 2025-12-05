e58f16fda474a5c9b800ccdf0801d38993c0b8be
jdolle <1841898+jdolle@users.noreply.github.com>
2025-12-05 13:02:23 -0800

    Hide unnecessary elements from header when screen is narrow

--- packages/web/app/src/components/get-started/trigger.tsx
+++ packages/web/app/src/components/get-started/trigger.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { FragmentType, graphql, useFragment } from '@/gql';
 import { getDocsUrl } from '@/lib/docs-url';
 import { useToggle } from '@/lib/hooks';
+import { cn } from '@/lib/utils';
 import { GetStartedWizard } from './wizard';
 
 const GetStartedWizard_GetStartedProgress = graphql(`
@@ -17,6 +18,7 @@ const GetStartedWizard_GetStartedProgress = graphql(`
 
 export function GetStartedProgress(props: {
   tasks: FragmentType<typeof GetStartedWizard_GetStartedProgress>;
+  className?: string;
 }): ReactElement | null {
   const [isOpen, toggle] = useToggle();
   const tasks = useFragment(GetStartedWizard_GetStartedProgress, props.tasks);
@@ -38,7 +40,10 @@ export function GetStartedProgress(props: {
     <>
       <button
         onClick={toggle}
-        className="cursor-pointer rounded px-4 py-2 text-left hover:opacity-80"
+        className={cn(
+          'cursor-pointer rounded px-4 py-2 text-left hover:opacity-80',
+          props.className,
+        )}
       >
         <div className="text-sm font-medium">Get Started</div>
         <div className="text-xs text-gray-500">
