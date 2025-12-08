fb831ea9c47d7fefaa495c1a58d84d0a2ba6ad5e
jdolle <1841898+jdolle@users.noreply.github.com>
2025-12-08 10:22:34 -0800

    fix migration typo

--- packages/migrations/src/actions/2025.12.30T00-00-00.schema-proposals-part-2.ts
+++ packages/migrations/src/actions/2025.12.30T00-00-00.schema-proposals-part-2.ts
@@ -15,7 +15,7 @@ export default {
           , created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
           -- reviews can also be tied to a stage transition event. If the review only contains comments, then this is null
           , stage_transition schema_proposal_stage NOT NULL
-          , author text NOT NULLL
+          , author text NOT NULL
           , schema_proposal_id UUID NOT NULL REFERENCES schema_proposals (id) ON DELETE CASCADE
           -- store the original text of the line that is being reviewed. If the base schema version changes, then this is
           -- used to determine which line this review falls on. If no line matches in the current version, then
