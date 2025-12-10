4bc545834eefced23b1d4d93364fb2599f261624
Laurin Quast <laurinquast@googlemail.com>
2025-12-10 21:49:24 +0100

    observability

--- packages/services/workflows/src/index.ts
+++ packages/services/workflows/src/index.ts
@@ -1,17 +1,16 @@
-import {
-  Logger as GraphileLogger,
-  LogLevel as GraphileLogLevel,
-  run,
-  Runner,
-} from 'graphile-worker';
+import { run } from 'graphile-worker';
 import { createPool } from 'slonik';
 import { Logger } from '@graphql-hive/logger';
+import { registerShutdown, startMetrics } from '@hive/service-common';
 import { Context } from './context.js';
 import { env } from './environment.js';
 import { createEmailProvider } from './lib/emails/providers.js';
+import { bridgeFastifyLogger, bridgeGraphileLogger } from './logger.js';
+import { createTaskEventEmitter } from './task-events.js';
 
-const pg = await createPool(env.postgres.connectionString);
-
+/**
+ * Registered Task Definitions.
+ */
 const modules = await Promise.all([
   import('./tasks/audit-log-export.js'),
   import('./tasks/email-verification.js'),
@@ -24,6 +23,12 @@ const modules = await Promise.all([
   import('./tasks/usage-rate-limit-warning.js'),
 ]);
 
+const crontab = `
+  # Purge expired schema checks every Sunday at 10:00AM
+  0 10 * * 0 purgeExpiredSchemaChecks
+`;
+
+const pg = await createPool(env.postgres.connectionString);
 const logger = new Logger({ level: env.log.level });
 
 logger.info({ pid: process.pid }, 'starting workflow service');
@@ -35,46 +40,37 @@ const context: Context = {
   requestBroker: env.requestBroker,
 };
 
-function logLevel(level: GraphileLogLevel) {
-  switch (level) {
-    case 'warning':
-      return 'warn' as const;
-    case 'info': {
-      return 'info' as const;
-    }
-    case 'debug': {
-      return 'debug' as const;
-    }
-    case 'error': {
-      return 'error' as const;
-    }
-  }
+const shutdownMetrics = env.prometheus
+  ? await startMetrics(env.prometheus.labels.instance, env.prometheus.port)
+  : null;
 
-  return 'info';
-}
-
-const runner: Runner = await run({
-  logger: new GraphileLogger(_scope => (level, message, _meta) => {
-    logger[logLevel(level)](message);
-  }),
-  crontab: `
-    # Purge expired schema checks every Saturday at 10:00AM
-    0 10 * * 0 purgeExpiredSchemaChecks
-  `,
+const runner = await run({
+  logger: bridgeGraphileLogger(logger),
+  crontab,
   pgPool: pg.pool,
   taskList: Object.fromEntries(modules.map(module => module.task(context))),
+  noHandleSignals: true,
+  events: createTaskEventEmitter(),
 });
 
-async function shutdown() {
-  try {
-    logger.info('Received shutdown signal. Stopping runner.');
-    await runner.stop();
-    logger.info('Runner shutdown successful.');
-    logger.info('Shutdown database connection.');
-    await pg.end();
-    logger.info('Shutdown database connection successful.');
-  } catch (error: unknown) {
-    logger.error({ error }, 'Unepected error occured');
-    process.exit(1);
-  }
-}
+registerShutdown({
+  logger: bridgeFastifyLogger(logger),
+  async onShutdown() {
+    try {
+      logger.info('Stopping task runner.');
+      await runner.stop();
+      logger.info('Task runner shutdown successful.');
+      logger.info('Shutdown postgres connection.');
+      await pg.end();
+      logger.info('Shutdown postgres connection successful.');
+      if (shutdownMetrics) {
+        logger.info('Stopping prometheus endpoint');
+        await shutdownMetrics();
+        logger.info('Stopping prometheus endpoint successful.');
+      }
+    } catch (error: unknown) {
+      logger.error({ error }, 'Unepected error occured');
+      process.exit(1);
+    }
+  },
+});
