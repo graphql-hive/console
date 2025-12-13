6ae46aee0b8c59c2c15e23065b1aba199d6aaf13
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-12-13 19:30:04 +0300

    Retry 3 times

--- integration-tests/tests/apollo-router/apollo-router.test.ts
+++ integration-tests/tests/apollo-router/apollo-router.test.ts
@@ -21,93 +21,98 @@ describe('Apollo Router Integration', () => {
         }
       });
     });
-  it('fetches the supergraph and sends usage reports', async () => {
-    const routerConfigPath = join(tmpdir(), `apollo-router-config-${Date.now()}.yaml`);
-    const { createOrg } = await initSeed().createOwner();
-    const { createProject } = await createOrg();
-    const { createTargetAccessToken, createCdnAccess, target, waitForOperationsCollected } =
-      await createProject(ProjectType.Federation);
-    const writeToken = await createTargetAccessToken({});
+  it(
+    'fetches the supergraph and sends usage reports',
+    {
+      retry: 3,
+    },
+    async () => {
+      const routerConfigPath = join(tmpdir(), `apollo-router-config-${Date.now()}.yaml`);
+      const { createOrg } = await initSeed().createOwner();
+      const { createProject } = await createOrg();
+      const { createTargetAccessToken, createCdnAccess, target, waitForOperationsCollected } =
+        await createProject(ProjectType.Federation);
+      const writeToken = await createTargetAccessToken({});
 
-    // Publish Schema
-    const publishSchemaResult = await writeToken
-      .publishSchema({
-        author: 'Arda',
-        commit: 'abc123',
-        sdl: /* GraphQL */ `
-          type Query {
-            me: User
-          }
-          type User {
-            id: ID!
-            name: String!
-          }
-        `,
-        service: 'users',
-        url: 'https://federation-demo.theguild.workers.dev/users',
-      })
-      .then(r => r.expectNoGraphQLErrors());
+      // Publish Schema
+      const publishSchemaResult = await writeToken
+        .publishSchema({
+          author: 'Arda',
+          commit: 'abc123',
+          sdl: /* GraphQL */ `
+            type Query {
+              me: User
+            }
+            type User {
+              id: ID!
+              name: String!
+            }
+          `,
+          service: 'users',
+          url: 'https://federation-demo.theguild.workers.dev/users',
+        })
+        .then(r => r.expectNoGraphQLErrors());
 
-    expect(publishSchemaResult.schemaPublish.__typename).toBe('SchemaPublishSuccess');
-    const cdnAccessResult = await createCdnAccess();
+      expect(publishSchemaResult.schemaPublish.__typename).toBe('SchemaPublishSuccess');
+      const cdnAccessResult = await createCdnAccess();
 
-    const usageAddress = await getServiceHost('usage', 8081);
+      const usageAddress = await getServiceHost('usage', 8081);
 
-    const routerBinPath = join(__dirname, '../../../target/debug/router');
-    if (!existsSync(routerBinPath)) {
-      throw new Error(
-        `Apollo Router binary not found at path: ${routerBinPath}, make sure to build it first with 'cargo build'`,
-      );
-    }
-    const routerPort = await getAvailablePort();
-    const routerConfigContent = `
+      const routerBinPath = join(__dirname, '../../../target/debug/router');
+      if (!existsSync(routerBinPath)) {
+        throw new Error(
+          `Apollo Router binary not found at path: ${routerBinPath}, make sure to build it first with 'cargo build'`,
+        );
+      }
+      const routerPort = await getAvailablePort();
+      const routerConfigContent = `
 supergraph:
   listen: 0.0.0.0:${routerPort}
 plugins:
   hive.usage: {}
 `.trim();
-    writeFileSync(routerConfigPath, routerConfigContent, 'utf-8');
-    const cdnEndpoint = await getServiceHost('server', 8082).then(
-      v => `http://${v}/artifacts/v1/${target.id}`,
-    );
-    const routerProc = execa(routerBinPath, ['--dev', '--config', routerConfigPath], {
-      all: true,
-      env: {
-        HIVE_CDN_ENDPOINT: cdnEndpoint,
-        HIVE_CDN_KEY: cdnAccessResult.secretAccessToken,
-        HIVE_ENDPOINT: `http://${usageAddress}`,
-        HIVE_TOKEN: writeToken.secret,
-      },
-    });
-    let log = '';
-    await new Promise((resolve, reject) => {
-      routerProc.catch(err => {
-        if (!err.isCanceled) {
-          reject(err);
-        }
-      });
-      const routerProcOut = routerProc.all;
-      if (!routerProcOut) {
-        return reject(new Error('No stdout from Apollo Router process'));
-      }
-      routerProcOut.on('data', data => {
-        log += data.toString();
-        if (log.includes('GraphQL endpoint exposed at')) {
-          resolve(true);
-        }
-      });
-    });
-
-    try {
-      const url = `http://localhost:${routerPort}/`;
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
+      writeFileSync(routerConfigPath, routerConfigContent, 'utf-8');
+      const cdnEndpoint = await getServiceHost('server', 8082).then(
+        v => `http://${v}/artifacts/v1/${target.id}`,
+      );
+      const routerProc = execa(routerBinPath, ['--dev', '--config', routerConfigPath], {
+        all: true,
+        env: {
+          HIVE_CDN_ENDPOINT: cdnEndpoint,
+          HIVE_CDN_KEY: cdnAccessResult.secretAccessToken,
+          HIVE_ENDPOINT: `http://${usageAddress}`,
+          HIVE_TOKEN: writeToken.secret,
         },
-        body: JSON.stringify({
-          query: `
+      });
+      let log = '';
+      await new Promise((resolve, reject) => {
+        routerProc.catch(err => {
+          if (!err.isCanceled) {
+            reject(err);
+          }
+        });
+        const routerProcOut = routerProc.all;
+        if (!routerProcOut) {
+          return reject(new Error('No stdout from Apollo Router process'));
+        }
+        routerProcOut.on('data', data => {
+          log += data.toString();
+          if (log.includes('GraphQL endpoint exposed at')) {
+            resolve(true);
+          }
+        });
+      });
+
+      try {
+        const url = `http://localhost:${routerPort}/`;
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: `
             query TestQuery {
               me {
                 id
@@ -115,26 +120,27 @@ plugins:
               }
             }
           `,
-        }),
-      });
+          }),
+        });
 
-      expect(response.status).toBe(200);
-      const result = await response.json();
-      expect(result).toEqual({
-        data: {
-          me: {
-            id: '1',
-            name: 'Ada Lovelace',
+        expect(response.status).toBe(200);
+        const result = await response.json();
+        expect(result).toEqual({
+          data: {
+            me: {
+              id: '1',
+              name: 'Ada Lovelace',
+            },
           },
-        },
-      });
-      await waitForOperationsCollected(1);
-    } catch (e) {
-      console.error('Router logs:\n', log);
-      throw e;
-    } finally {
-      routerProc.cancel();
-      rmSync(routerConfigPath);
-    }
-  });
+        });
+        await waitForOperationsCollected(1);
+      } catch (e) {
+        console.error('Router logs:\n', log);
+        throw e;
+      } finally {
+        routerProc.cancel();
+        rmSync(routerConfigPath);
+      }
+    },
+  );
 });
