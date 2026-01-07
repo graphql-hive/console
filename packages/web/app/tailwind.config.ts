f396c587c9a023f25d95e35c9dd305d8a6853e7e
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-01-07 15:43:56 -0600

    remove tailwind radix package and utilities

--- packages/web/app/tailwind.config.ts
+++ packages/web/app/tailwind.config.ts
@@ -1,7 +1,6 @@
 import svgToDataUri from 'mini-svg-data-uri';
 import type { Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
-import tailwindcssRadix from 'tailwindcss-radix';
 import colors from 'tailwindcss/colors';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
@@ -158,19 +157,6 @@ const config: Config = {
         ...theme('colors'),
       }),
       keyframes: {
-        // Dropdown menu
-        'scale-in': {
-          '0%': { opacity: '0', transform: 'scale(0)' },
-          '100%': { opacity: '1', transform: 'scale(1)' },
-        },
-        'slide-down': {
-          '0%': { opacity: '0', transform: 'translateY(-10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-        'slide-up': {
-          '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
         // Tooltip
         'slide-up-fade': {
           '0%': { opacity: '0', transform: 'translateY(2px)' },
@@ -187,68 +173,6 @@ const config: Config = {
         'slide-left-fade': {
           '0%': { opacity: '0', transform: 'translateX(2px)' },
           '100%': { opacity: '1', transform: 'translateX(0)' },
-        },
-        // Navigation menu
-        'enter-from-right': {
-          '0%': { transform: 'translateX(200px)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        'enter-from-left': {
-          '0%': { transform: 'translateX(-200px)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        'exit-to-right': {
-          '0%': { transform: 'translateX(0)', opacity: '1' },
-          '100%': { transform: 'translateX(200px)', opacity: '0' },
-        },
-        'exit-to-left': {
-          '0%': { transform: 'translateX(0)', opacity: '1' },
-          '100%': { transform: 'translateX(-200px)', opacity: '0' },
-        },
-        'scale-in-content': {
-          '0%': { transform: 'rotateX(-30deg) scale(0.9)', opacity: '0' },
-          '100%': { transform: 'rotateX(0deg) scale(1)', opacity: '1' },
-        },
-        'scale-out-content': {
-          '0%': { transform: 'rotateX(0deg) scale(1)', opacity: '1' },
-          '100%': { transform: 'rotateX(-10deg) scale(0.95)', opacity: '0' },
-        },
-        'fade-in': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        'fade-out': {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0' },
-        },
-        // Toast
-        'toast-hide': {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0' },
-        },
-        'toast-slide-in-right': {
-          '0%': { transform: 'translateX(calc(100% + 1rem))' },
-          '100%': { transform: 'translateX(0)' },
-        },
-        'toast-slide-in-bottom': {
-          '0%': { transform: 'translateY(calc(100% + 1rem))' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        'toast-swipe-out-x': {
-          '0%': { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
-          '100%': { transform: 'translateX(calc(100% + 1rem))' },
-        },
-        'toast-swipe-out-y': {
-          '0%': { transform: 'translateY(var(--radix-toast-swipe-end-y))' },
-          '100%': { transform: 'translateY(calc(100% + 1rem))' },
-        },
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
         },
         shimmer: {
           from: {
@@ -275,32 +199,10 @@ const config: Config = {
         },
       },
       animation: {
-        // Dropdown menu
-        'scale-in': 'scale-in 0.2s ease-in-out',
-        'slide-down': 'slide-down 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-        'slide-up': 'slide-up 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-        // Tooltip
         'slide-up-fade': 'slide-up-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
         'slide-right-fade': 'slide-right-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
         'slide-down-fade': 'slide-down-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
         'slide-left-fade': 'slide-left-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-        // Navigation menu
-        'enter-from-right': 'enter-from-right 0.25s ease',
-        'enter-from-left': 'enter-from-left 0.25s ease',
-        'exit-to-right': 'exit-to-right 0.25s ease',
-        'exit-to-left': 'exit-to-left 0.25s ease',
-        'scale-in-content': 'scale-in-content 0.2s ease',
-        'scale-out-content': 'scale-out-content 0.2s ease',
-        'fade-in': 'fade-in 0.2s ease',
-        'fade-out': 'fade-out 0.2s ease',
-        // Toast
-        'toast-hide': 'toast-hide 100ms ease-in forwards',
-        'toast-slide-in-right': 'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        'toast-slide-in-bottom': 'toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-        'toast-swipe-out-x': 'toast-swipe-out-x 100ms ease-out forwards',
-        'toast-swipe-out-y': 'toast-swipe-out-y 100ms ease-out forwards',
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
         shimmer: 'shimmer 1.5s linear infinite',
         /** @source https://gist.github.com/krishaantechnology/245b29cfbb25eb456c09fce63673decc */
         shake: 'shake 0.82s cubic-bezier(.36,.07,.19,.97) both',
@@ -312,7 +214,6 @@ const config: Config = {
   },
   plugins: [
     // Utilities and variants for styling Radix state
-    tailwindcssRadix({}),
     tailwindcssAnimate,
     plugin(({ addBase, theme }) => {
       const allColors = flattenColorPalette(theme('colors'));
