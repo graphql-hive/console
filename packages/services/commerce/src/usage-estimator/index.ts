0ce9c82b810fef311f3856bb90b2e2a1823a7101
jdolle <1841898+jdolle@users.noreply.github.com>
2026-01-08 14:08:45 -0800

    Evaluate usageEstimation year input range at runtime (#7459)

--- packages/services/commerce/src/usage-estimator/index.ts
+++ packages/services/commerce/src/usage-estimator/index.ts
@@ -7,20 +7,25 @@ export const usageEstimatorRouter = router({
       z
         .object({
           month: z.number().min(1).max(12),
-          year: z
-            .number()
-            .min(new Date().getFullYear() - 1)
-            .max(new Date().getFullYear()),
+          year: z.any(),
           organizationId: z.string().min(1),
         })
         .required(),
     )
     .query(async ({ ctx, input }) => {
+      // The range must be defined at runtime to avoid issues at the end of the year
+      const now = new Date();
+      const rangeValidation = z
+        .number()
+        .min(now.getFullYear() - 1)
+        .max(new Date(now.setMonth(now.getMonth() + 1)).getFullYear());
+      const year = rangeValidation.parse(input.year);
+
       const estimationResponse =
         await ctx.usageEstimator.estimateCollectedOperationsForOrganization({
           organizationId: input.organizationId,
           month: input.month,
-          year: input.year,
+          year,
         });
 
       if (!estimationResponse.data.length) {
