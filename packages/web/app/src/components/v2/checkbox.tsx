258959f6be2f6f4c378ba7f81fb5877f8d28ad24
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-02-03 14:43:24 -0600

    clean up fix styles

--- packages/web/app/src/components/v2/checkbox.tsx
+++ packages/web/app/src/components/v2/checkbox.tsx
@@ -8,11 +8,11 @@ export const Checkbox = (props: CheckboxProps): ReactElement => {
     <Root
       {...props}
       className={cn(
-        'bg-neutral-5 border-neutral-2 text-neutral-2 disabled:border-neutral-2 hover:border-accent_30 flex size-5 shrink-0 items-center justify-center rounded-sm border disabled:cursor-not-allowed',
+        'bg-neutral-5 text-neutral-2 disabled:border-neutral-2 hover:border-accent_30! data-[state=checked]:bg-accent_10 data-[state=checked]:border-accent_10 flex size-5 shrink-0 items-center justify-center rounded-sm border border-transparent disabled:cursor-not-allowed',
         props.className,
       )}
     >
-      <Indicator className="data-[state=checked]:bg-accent_10 data-[state=checked]:border-accent_10 flex size-full items-center justify-center border-none">
+      <Indicator className="flex size-full items-center justify-center">
         <CheckIcon className="text-accent" />
       </Indicator>
     </Root>
