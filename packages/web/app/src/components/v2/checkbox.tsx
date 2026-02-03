5b19cffa4fe2897392161bfda75f1f0e7b330e4e
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-02-03 13:45:34 -0600

    fix checkbox UI states

--- packages/web/app/src/components/v2/checkbox.tsx
+++ packages/web/app/src/components/v2/checkbox.tsx
@@ -8,12 +8,12 @@ export const Checkbox = (props: CheckboxProps): ReactElement => {
     <Root
       {...props}
       className={cn(
-        'bg-neutral-5 border-neutral-2 text-neutral-2 disabled:border-neutral-2 flex size-5 shrink-0 items-center justify-center rounded-sm border hover:border-orange-700 disabled:cursor-not-allowed',
+        'bg-neutral-5 border-neutral-2 text-neutral-2 disabled:border-neutral-2 hover:border-accent_30 flex size-5 shrink-0 items-center justify-center rounded-sm border disabled:cursor-not-allowed',
         props.className,
       )}
     >
-      <Indicator className="flex size-full items-center justify-center bg-current">
-        <CheckIcon className="text-neutral-1" />
+      <Indicator className="data-[state=checked]:bg-accent_10 data-[state=checked]:border-accent_10 flex size-full items-center justify-center border-none">
+        <CheckIcon className="text-accent" />
       </Indicator>
     </Root>
   );
