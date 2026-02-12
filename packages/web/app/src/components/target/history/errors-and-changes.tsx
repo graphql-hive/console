abb29fd8548e37ae3ded637bc2ce688191d341bb
Adam Benhassen <adambenhassen@hotmail.com>
2026-02-12 15:23:02 +0100

    remove unused import

--- packages/web/app/src/components/target/history/errors-and-changes.tsx
+++ packages/web/app/src/components/target/history/errors-and-changes.tsx
@@ -11,7 +11,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { PulseIcon } from '@/components/ui/icon';
