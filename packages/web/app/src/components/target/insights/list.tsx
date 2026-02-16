a762fc64ec7ce1c22929ba82e83e5a37fd22e0b6
Adam Benhassen <adambenhassen@hotmail.com>
2026-02-16 23:15:04 +0100

    remove otherColumnSorted prop

--- packages/web/app/src/components/target/insights/list.tsx
+++ packages/web/app/src/components/target/insights/list.tsx
@@ -258,7 +258,6 @@ function OperationsTable({
                       <Sortable
                         sortOrder={header.column.getIsSorted()}
                         onClick={header.column.getToggleSortingHandler()}
-                        otherColumnSorted={sortedColumnsById.some(id => id !== header.id)}
                       >
                         {name}
                       </Sortable>
