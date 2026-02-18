2cfe776cc52a4ab72b4feea10632665c095ccc1b
Laurin Quast <laurinquast@googlemail.com>
2026-02-18 10:45:46 +0100

    some password validation

--- packages/services/server/src/supertokens-at-home/shared.ts
+++ packages/services/server/src/supertokens-at-home/shared.ts
@@ -9,3 +9,34 @@ export const SuperTokensSessionPayloadV2Model = z.object({
 });
 
 export type SuperTokensSessionPayload = z.TypeOf<typeof SuperTokensSessionPayloadV2Model>;
+
+export function validatePassword(password: string):
+  | {
+      status: 'OK';
+    }
+  | {
+      status: 'INVALID';
+      message: string;
+    } {
+  if (password.length < 8) {
+    return { status: 'INVALID', message: 'Password must be at least 8 characters long.' };
+  }
+
+  if ((password.match(/[A-Z]/g) || []).length < 2) {
+    return { status: 'INVALID', message: 'Password must contain at least two uppercase letters.' };
+  }
+
+  if ((password.match(/[!@#$%^&*(),.?":{}|<>]/g) || []).length < 1) {
+    return { status: 'INVALID', message: 'Password must contain at least one special character.' };
+  }
+
+  if ((password.match(/[0-9]/g) || []).length < 2) {
+    return { status: 'INVALID', message: 'Password must contain at least two digits.' };
+  }
+
+  if ((password.match(/[a-z]/g) || []).length < 2) {
+    return { status: 'INVALID', message: 'Password must contain at least two lowercase letters.' };
+  }
+
+  return { status: 'OK' };
+}
