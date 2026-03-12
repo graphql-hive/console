9edd55e25d6d1ba60dba918b94246aabc507c7cd
Laurin Quast <laurinquast@googlemail.com>
2026-03-12 13:18:13 +0100

    add app_deployment_documents.target_id column

---
+++ packages/migrations/src/clickhouse-actions/017-affected-app-deployments-performance.ts
@@ -0,0 +1,35 @@
+import type { Action } from '../clickhouse';
+
+export const action: Action = async exec => {
+  await exec(`
+    ALTER TABLE app_deployment_documents
+    ADD COLUMN target_id LowCardinality(String)
+    ;
+  `);
+
+  await exec(`
+    CREATE TABLE "app_deployment_document_coordinates" (
+      "target_id" LowCardinality(String)
+      , "coordinate" LowCardinality(String)
+      , "app_deployment_id" LowCardinality(String)
+      , "document_hash" String
+      , "operation_name" String
+    )
+    ENGINE = ReplacingMergeTree
+    ORDER BY ("target_id", "coordinate", "app_deployment_id", "document_hash")
+    ;
+  `);
+
+  await exec(`
+    CREATE MATERIALIZED VIEW "mv_documents_by_coordinate"
+    TO "app_deployment_document_coordinates"
+    AS
+    SELECT
+      "target_id"
+      , arrayJoin("schema_coordinates") AS "schema_coordinate"
+      , "app_deployment_id"
+      , "document_hash"
+    FROM "app_deployment_documents"
+    ;
+  `);
+};
