046d7514807a8a3a4e28f7704693d09dea3af8b9
jdolle <1841898+jdolle@users.noreply.github.com>
2026-03-18 14:00:43 -0700

    Add back startup probe

--- deployment/services/usage.ts
+++ deployment/services/usage.ts
@@ -56,6 +56,7 @@ export function deployUsage({
           endpoint: '/_readiness',
         },
         livenessProbe: '/_health',
+        startupProbe: '/_health',
         availabilityOnEveryNode: true,
         env: {
           ...environment.envVars,
