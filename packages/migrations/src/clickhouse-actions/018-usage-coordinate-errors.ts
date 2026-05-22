ab312dee49fbfb6bda50c14f9d733c56918be837
jdolle <1841898+jdolle@users.noreply.github.com>
2026-05-22 16:15:02 -0700

    Add projection rebuild mode

--- packages/migrations/src/clickhouse-actions/018-usage-coordinate-errors.ts
+++ packages/migrations/src/clickhouse-actions/018-usage-coordinate-errors.ts
@@ -65,7 +65,7 @@ export const action: Action = async exec => {
     ORDER BY (target, coordinate, code, timestamp, hash, expires_at)
     -- only store for 24hr because after that, the hourly or daily table will be used
     TTL least(timestamp + toIntervalHour(24), expires_at)
-    SETTINGS index_granularity = 8192
+    SETTINGS index_granularity = 8192, deduplicate_merge_projection_mode = 'rebuild'
     ;
   `);
 
@@ -123,7 +123,7 @@ export const action: Action = async exec => {
     ORDER BY (target, coordinate, code, timestamp, hash, expires_at)
     -- keep for a maximum of 30 days because after that the relative time range will only use the daily calculations
     TTL least(timestamp + toIntervalDay(30), expires_at)
-    SETTINGS index_granularity = 8192
+    SETTINGS index_granularity = 8192, deduplicate_merge_projection_mode = 'rebuild'
     ;
   `);
   await exec(`
@@ -177,7 +177,7 @@ export const action: Action = async exec => {
     PRIMARY KEY (target, coordinate, code, timestamp, hash)
     ORDER BY (target, coordinate, code, timestamp, hash, expires_at)
     TTL expires_at
-    SETTINGS index_granularity = 8192
+    SETTINGS index_granularity = 8192, deduplicate_merge_projection_mode = 'rebuild'
     ;
   `);
 
