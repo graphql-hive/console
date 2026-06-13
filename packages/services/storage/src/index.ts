d89b28cc250d869b1481ee754cd2e82064b7f103
Laurin Quast <laurinquast@googlemail.com>
2026-06-13 11:48:18 +0200

    feat: restrict SCIM provisioning to only domains verified with the organizationand add a flag to enforce login through oidc

--- packages/services/storage/src/index.ts
+++ packages/services/storage/src/index.ts
@@ -4023,6 +4023,10 @@ const OIDCIntegrationBaseModel = z.object({
   defaultAssignedResources: z.any().nullable(),
   requireInvitation: z.boolean(),
   userIdClaim: z.string().nullable(),
+  /** Whether a user needs to be provisioned before login. */
+  userProvisioningRequired: z.boolean(),
+  /** Whether a user with a verified domain needs to log in through the OIDC flow. */
+  oidcForVerifiedDomainsRequired: z.boolean(),
 });
 
 const OIDCIntegrationLegacyModel = OIDCIntegrationBaseModel.extend({
@@ -4042,6 +4046,8 @@ const OIDCIntegrationLegacyModel = OIDCIntegrationBaseModel.extend({
   defaultMemberRoleId: record.defaultRoleId,
   defaultResourceAssignment: record.defaultAssignedResources,
   userIdClaim: record.userIdClaim ?? 'sub',
+  userProvisioningRequired: record.userProvisioningRequired,
+  oidcForVerifiedDomainsRequired: record.oidcForVerifiedDomainsRequired,
 }));
 
 const LatestOIDCIntegrationModel = OIDCIntegrationBaseModel.extend({
@@ -4064,6 +4070,8 @@ const LatestOIDCIntegrationModel = OIDCIntegrationBaseModel.extend({
   defaultMemberRoleId: record.defaultRoleId,
   defaultResourceAssignment: record.defaultAssignedResources,
   userIdClaim: record.userIdClaim ?? 'sub',
+  userProvisioningRequired: record.userProvisioningRequired,
+  oidcForVerifiedDomainsRequired: record.oidcForVerifiedDomainsRequired,
 }));
 
 const OIDCIntegrationModel = z.union([OIDCIntegrationLegacyModel, LatestOIDCIntegrationModel]);
@@ -4473,6 +4481,8 @@ const oidcIntegrationFields = (prefix: TaggedTemplateLiteralInvocation = psql``)
   , ${prefix}"default_assigned_resources" AS "defaultAssignedResources"
   , ${prefix}"require_invitation" AS "requireInvitation"
   , ${prefix}"user_id_claim" AS "userIdClaim"
+  , ${prefix}"user_provisioning_required" AS "userProvisioningRequired"
+  , ${prefix}"oidc_for_verified_domains_required" AS "oidcForVerifiedDomainsRequired"
 `;
 
 const OrganizationModel = z
