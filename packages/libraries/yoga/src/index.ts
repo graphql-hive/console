f51db5dd5a9288721fb095b1668844814ad5ee7e
enisdenjo <badurinadenis@gmail.com>
2026-07-01 16:40:12 +0100

    Revert "fix try"
    This reverts commit 0c97e8fbbf0efa2938c9b6e83171b501b001426a.

--- packages/libraries/yoga/src/index.ts
+++ packages/libraries/yoga/src/index.ts
@@ -89,10 +89,9 @@ export function useHive(clientOrOptions: HiveClient | HivePluginOptions): Plugin
         }
       };
     },
-    onExecute(context) {
-      const args = stabilizeArgs(context);
+    onExecute() {
       return {
-        onExecuteDone({ result }) {
+        onExecuteDone({ args, result }) {
           const record = contextualCache.get(args.contextValue);
           if (!record) {
             return;
@@ -138,12 +137,12 @@ export function useHive(clientOrOptions: HiveClient | HivePluginOptions): Plugin
     },
     onSubscribe(context) {
       const record = contextualCache.get(context.args.contextValue);
-      const args = stabilizeArgs(context);
+
       return {
         onSubscribeResult() {
           const experimental__persistedDocumentHash = record?.experimental__documentId;
           hive.collectSubscriptionUsage({
-            args,
+            args: context.args,
             experimental__persistedDocumentHash,
           });
         },
@@ -303,15 +302,4 @@ export function useHive(clientOrOptions: HiveClient | HivePluginOptions): Plugin
       }
     },
   };
-}
-
-// some envelop plugins (like extended-validation) mutate documents in place and
-// can therefore cause the document to be different AFTER execution than the one that
-// was used FOR execution. to avoid this rugpull, we create a new document object for
-// usage reporting.
-// TODO: this depends on the extended-validation running AFTER this plugin
-function stabilizeArgs<A extends { document: DocumentNode }>(ctx: { args: A }) {
-  const args = { ...ctx.args };
-  if (args.document) args.document = { ...ctx.args.document };
-  return args;
 }
