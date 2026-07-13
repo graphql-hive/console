b1ad5369510cb45dbf27f910d81c3643271d9fd5
ben-m-klein <bklein@sfsu.edu>
2026-07-13 16:36:48 -0700

    Update packages/migrations/src/actions/2026.06.22T20-10-00.discord-webhook.ts
    Co-authored-by: jdolle <1841898+jdolle@users.noreply.github.com>

--- packages/migrations/src/actions/2026.06.22T20-10-00.discord-webhook.ts
+++ packages/migrations/src/actions/2026.06.22T20-10-00.discord-webhook.ts
@@ -3,6 +3,6 @@ import { type MigrationExecutor } from '../pg-migrator';
 export default {
   name: '2026.06.22T20-10-00.discord-webhook.ts',
   run: ({ psql }) => psql`
-    ALTER TYPE alert_channel_type ADD VALUE 'DISCORD_WEBHOOK';
+    ALTER TYPE alert_channel_type ADD VALUE IF NOT EXISTS 'DISCORD_WEBHOOK';
   `,
 } satisfies MigrationExecutor;
