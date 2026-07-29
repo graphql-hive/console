979b670b079a864abc40520c68200eb1f8dc5e22
jdolle <1841898+jdolle@users.noreply.github.com>
2026-07-28 18:16:00 -0700

    Fix import

--- packages/libraries/core/src/client/subrequests/extract-coordinates.ts
+++ packages/libraries/core/src/client/subrequests/extract-coordinates.ts
@@ -12,7 +12,7 @@ import {
   OperationDefinitionNode,
   SelectionSetNode,
 } from 'graphql';
-import { HIVE_INTERNAL_TYPENAME } from '../add-typenames';
+import { HIVE_INTERNAL_TYPENAME } from '../add-typenames.js';
 
 export type CoordinateMap = Record<string, number>;
 interface FieldPlan {
