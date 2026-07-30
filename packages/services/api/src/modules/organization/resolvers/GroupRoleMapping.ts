e6cdfa631db8fc2153bbc2948210a0a86d322178
Laurin Quast <laurinquast@googlemail.com>
2026-07-30 17:32:48 +0200

    remove codegen comments

--- packages/services/api/src/modules/organization/resolvers/GroupRoleMapping.ts
+++ packages/services/api/src/modules/organization/resolvers/GroupRoleMapping.ts
@@ -2,15 +2,6 @@ import { OrganizationMemberRoles } from '../providers/organization-member-roles'
 import { ResourceAssignments } from '../providers/resource-assignments';
 import type { GroupRoleMappingResolvers } from './../../../__generated__/types';
 
-/*
- * Note: This object type is generated because "GroupRoleMappingMapper" is declared. This is to ensure runtime safety.
- *
- * When a mapper is used, it is possible to hit runtime errors in some scenarios:
- * - given a field name, the schema type's field type does not match mapper's field type
- * - or a schema type's field does not exist in the mapper's fields
- *
- * If you want to skip this file generation, remove the mapper or update the pattern in the `resolverGeneration.object` config.
- */
 export const GroupRoleMapping: GroupRoleMappingResolvers = {
   async resourceAssignment(mapping, _, { injector }) {
     return injector.get(ResourceAssignments).resolveGraphQLMemberResourceAssignment({
