40de2f8d1b8d6251447055d4577a5d32bd21954c
jdolle <1841898+jdolle@users.noreply.github.com>
2026-08-10 16:00:18 -0700

    Only expose defaultCollections

--- packages/libraries/render-laboratory/src/index.ts
+++ packages/libraries/render-laboratory/src/index.ts
@@ -1,5 +1,5 @@
 import type { GraphiQLOptions } from 'graphql-yoga';
-import type { LaboratoryProps } from '@graphql-hive/laboratory';
+import type { LaboratoryCollection, LaboratoryProps } from '@graphql-hive/laboratory';
 import {
   editorWorkerService,
   favicon,
@@ -10,7 +10,7 @@ import {
   typescriptWorker,
 } from './laboratory.js';
 
-const mapGraphiQLOptionsToLaboratoryProps = (opts?: GraphiQLOptions): LaboratoryProps => {
+const mapGraphiQLOptionsToLaboratoryProps = (opts?: RenderLaboratoryOptions): LaboratoryProps => {
   if (!opts) {
     return {};
   }
@@ -30,87 +30,75 @@ const mapGraphiQLOptionsToLaboratoryProps = (opts?: GraphiQLOptions): Laboratory
         schemaDescription: opts.schemaDescription,
       },
     },
+    defaultCollections: opts.defaultCollections,
   } satisfies LaboratoryProps;
 };
 
-function isLaboratoryProps(opts: GraphiQLOptions | LaboratoryProps): opts is LaboratoryProps {
-  return (
-    'defaultEndpoint' in opts ||
-    'onEndpointChange' in opts ||
-    'defaultCollections' in opts ||
-    'defaultOperations' in opts ||
-    'defaultHistory' in opts ||
-    'defaultPreflight' in opts ||
-    'permissions' in opts
-  );
-}
-
-export const renderLaboratory = (
-  opts?: GraphiQLOptions | (LaboratoryProps & { title?: string }),
-) => {
-  let options: LaboratoryProps | undefined =
-    !opts || isLaboratoryProps(opts) ? opts : mapGraphiQLOptionsToLaboratoryProps(opts);
-
-  return /* HTML */ `
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <title>${opts?.title || 'Hive Laboratory'}</title>
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          media="(prefers-color-scheme: light)"
-          href="${favicon}"
-        />
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          media="(prefers-color-scheme: dark)"
-          href="${faviconDark}"
-        />
-        <style>
-          html,
-          body,
-          #root {
-            height: 100%;
-          }
-
-          body {
-            margin: 0;
-          }
-        </style>
-      </head>
-      <body id="body" class="no-focus-outline">
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        <div id="root"></div>
-
-        <script>
-          function prepareBlob(workerContent) {
-            const blob = new Blob([workerContent], { type: 'application/javascript' });
-            return URL.createObjectURL(blob);
-          }
-          const workers = {
-            editorWorkerService: prepareBlob(${JSON.stringify(editorWorkerService)}),
-            typescript: prepareBlob(${JSON.stringify(typescriptWorker)}),
-            json: prepareBlob(${JSON.stringify(jsonWorker)}),
-            graphql: prepareBlob(${JSON.stringify(graphqlWorker)}),
-          };
-          self['MonacoEnvironment'] = {
-            globalAPI: false,
-            getWorkerUrl: function (moduleId, label) {
-              return workers[label];
-            },
-          };
-
-          ${js};
-
-          HiveLaboratory.renderLaboratory(
-            window.document.querySelector('#root'),
-            ${JSON.stringify(options)},
-          );
-        </script>
-      </body>
-    </html>
-  `;
+type LaboratoryOptions = {
+  defaultCollections?: LaboratoryCollection[];
 };
+
+type RenderLaboratoryOptions = GraphiQLOptions & LaboratoryOptions;
+
+export const renderLaboratory = (opts?: RenderLaboratoryOptions) => /* HTML */ `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <title>${opts?.title || 'Hive Laboratory'}</title>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        media="(prefers-color-scheme: light)"
+        href="${favicon}"
+      />
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        media="(prefers-color-scheme: dark)"
+        href="${faviconDark}"
+      />
+      <style>
+        html,
+        body,
+        #root {
+          height: 100%;
+        }
+
+        body {
+          margin: 0;
+        }
+      </style>
+    </head>
+    <body id="body" class="no-focus-outline">
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root"></div>
+
+      <script>
+        function prepareBlob(workerContent) {
+          const blob = new Blob([workerContent], { type: 'application/javascript' });
+          return URL.createObjectURL(blob);
+        }
+        const workers = {
+          editorWorkerService: prepareBlob(${JSON.stringify(editorWorkerService)}),
+          typescript: prepareBlob(${JSON.stringify(typescriptWorker)}),
+          json: prepareBlob(${JSON.stringify(jsonWorker)}),
+          graphql: prepareBlob(${JSON.stringify(graphqlWorker)}),
+        };
+        self['MonacoEnvironment'] = {
+          globalAPI: false,
+          getWorkerUrl: function (moduleId, label) {
+            return workers[label];
+          },
+        };
+
+        ${js};
+
+        HiveLaboratory.renderLaboratory(
+          window.document.querySelector('#root'),
+          ${JSON.stringify(mapGraphiQLOptionsToLaboratoryProps(opts))},
+        );
+      </script>
+    </body>
+  </html>
+`;
