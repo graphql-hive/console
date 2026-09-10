c6f22a0077a9b45b499a9a9d17ae4437991b52e2
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-09-10 18:31:16 -0500

    inventory preview for the toast system

--- packages/web/app/foundry.config.ts
+++ packages/web/app/foundry.config.ts
@@ -79,6 +79,7 @@ export default defineConfig({
         { label: 'Select' },
         { label: 'Switch' },
         { label: 'Textarea' },
+        { label: 'Toast' },
         { label: 'Tooltip' },
       ],
     },
