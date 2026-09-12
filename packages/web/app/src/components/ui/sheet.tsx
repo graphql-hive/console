5c1cfca28c42420166ff05af6c1bda0d6506f2f1
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2026-09-11 20:54:57 -0500

    prettier

--- packages/web/app/src/components/ui/sheet.tsx
+++ packages/web/app/src/components/ui/sheet.tsx
@@ -52,8 +52,7 @@ const sheetVariants = cva(
 );
 
 interface SheetContentProps
-  extends
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
+  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
     VariantProps<typeof sheetVariants> {
   noOverlay?: boolean;
 }
